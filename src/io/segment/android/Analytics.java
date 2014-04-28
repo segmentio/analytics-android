4e325353d8d3b52f3207aac192d7bed58240ce0d
ivolo <ivolo@mit.edu>
2014-04-27 21:18:22 -0400

    publush 0.6.8

--- src/io/segment/android/Analytics.java
+++ src/io/segment/android/Analytics.java
@@ -46,7 +46,7 @@ import android.text.TextUtils;
 
 public class Analytics {
 	
-	public static final String VERSION = "0.6.7";
+	public static final String VERSION = "0.6.8";
 	
 	private static AnalyticsStatistics statistics;
 	
