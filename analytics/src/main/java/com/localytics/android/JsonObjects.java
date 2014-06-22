e708451fff730a0dc8830c3327830faa8f1faa19
Prateek Srivastava <f2prateek@gmail.com>
2014-06-22 12:13:05 -0700

    Add Copyright Notices

--- analytics/src/main/java/com/localytics/android/JsonObjects.java
+++ analytics/src/main/java/com/localytics/android/JsonObjects.java
@@ -1,9 +1,28 @@
-// @formatter:off
 /*
- * JsonObjects.java Copyright (C) 2013 Char Software Inc., DBA Localytics. This code is provided under the Localytics
- * Modified BSD License. A copy of this license has been distributed in a file called LICENSE with this source code. Please visit
- * www.localytics.com for more information.
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Segment.io, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
+
+// @formatter:off
 // @formatter:on
 
 package com.localytics.android;
