8fae38fb1146f01d350d2d6fae3341926ad31c88
Prateek Srivastava <f2prateek@gmail.com>
2014-11-18 13:52:01 -0700

    Migrate Bugsnag Tests

--- core/src/androidTest/java/com/segment/analytics/BugsnagRobolectricTest.java
+++ core/src/androidTest/java/com/segment/analytics/BugsnagRobolectricTest.java
@@ -1,5 +1,7 @@
 package com.segment.analytics;
 
+import android.app.Activity;
+import android.os.Bundle;
 import com.bugsnag.android.Bugsnag;
 import org.junit.Before;
 import org.junit.Rule;
@@ -12,14 +14,20 @@ import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import static com.segment.analytics.TestUtils.AliasPayloadBuilder;
+import static com.segment.analytics.TestUtils.GroupPayloadBuilder;
+import static com.segment.analytics.TestUtils.IdentifyPayloadBuilder;
+import static com.segment.analytics.TestUtils.ScreenPayloadBuilder;
+import static com.segment.analytics.TestUtils.TrackPayloadBuilder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.verifyNoMoreInteractions;
 import static org.powermock.api.mockito.PowerMockito.verifyStatic;
-import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(RobolectricTestRunner.class) @Config(emulateSdk = 18, manifest = Config.NONE)
 @PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*" })
 @PrepareForTest(Bugsnag.class)
-public class BugsnagRobolectricTest extends IntegrationRobolectricExam {
+public class BugsnagRobolectricTest extends AbstractIntegrationTest {
   @Rule public PowerMockRule rule = new PowerMockRule();
   BugsnagIntegration integration;
 
@@ -29,7 +37,7 @@ public class BugsnagRobolectricTest extends IntegrationRobolectricExam {
     integration = new BugsnagIntegration();
   }
 
-  @Test public void initialize() throws IllegalStateException {
+  @Test @Override public void initialize() throws IllegalStateException {
     integration.initialize(context,
         new JsonMap().putValue("apiKey", "foo").putValue("useSSL", true), true);
     verifyStatic();
@@ -37,8 +45,9 @@ public class BugsnagRobolectricTest extends IntegrationRobolectricExam {
     Bugsnag.setUseSSL(true);
   }
 
-  @Test
-  public void onActivityCreated() {
+  @Test @Override public void activityCreate() {
+    Activity activity = mock(Activity.class);
+    Bundle bundle = mock(Bundle.class);
     when(activity.getLocalClassName()).thenReturn("foo");
     integration.onActivityCreated(activity, bundle);
     verifyStatic();
@@ -47,35 +56,52 @@ public class BugsnagRobolectricTest extends IntegrationRobolectricExam {
     Bugsnag.onActivityCreate(activity);
   }
 
-  @Test public void onActivityResumed() {
-    integration.onActivityResumed(activity);
-    verifyStatic();
-    Bugsnag.onActivityResume(activity);
-  }
-
-  @Test public void onActivityPaused() {
-    integration.onActivityPaused(activity);
-    verifyStatic();
-    Bugsnag.onActivityPause(activity);
-  }
-
-  @Test public void onActivityDestroyed() {
-    integration.onActivityDestroyed(activity);
-    verifyStatic();
-    Bugsnag.onActivityDestroy(activity);
-  }
-
-  @Test public void activityLifecycle() {
-    integration.onActivityStopped(activity);
-    integration.onActivitySaveInstanceState(activity, bundle);
+  @Test @Override public void activityStart() {
+    Activity activity = mock(Activity.class);
     integration.onActivityStarted(activity);
     verifyStatic();
     verifyNoMoreInteractions(Bugsnag.class);
   }
 
-  @Test public void identify() {
-    traits.putUserId("foo").putEmail("bar").putName("baz");
-    integration.identify(identifyPayload("foo"));
+  @Test @Override public void activityResume() {
+    Activity activity = mock(Activity.class);
+    integration.onActivityResumed(activity);
+    verifyStatic();
+    Bugsnag.onActivityResume(activity);
+  }
+
+  @Test @Override public void activityPause() {
+    Activity activity = mock(Activity.class);
+    integration.onActivityPaused(activity);
+    verifyStatic();
+    Bugsnag.onActivityPause(activity);
+  }
+
+  @Test @Override public void activityStop() {
+    Activity activity = mock(Activity.class);
+    integration.onActivityStopped(activity);
+    verifyStatic();
+    verifyNoMoreInteractions(Bugsnag.class);
+  }
+
+  @Test @Override public void activitySaveInstance() {
+    Activity activity = mock(Activity.class);
+    Bundle bundle = mock(Bundle.class);
+    integration.onActivitySaveInstanceState(activity, bundle);
+    verifyStatic();
+    verifyNoMoreInteractions(Bugsnag.class);
+  }
+
+  @Test @Override public void activityDestroy() {
+    Activity activity = mock(Activity.class);
+    integration.onActivityDestroyed(activity);
+    verifyStatic();
+    Bugsnag.onActivityDestroy(activity);
+  }
+
+  @Test @Override public void identify() {
+    Traits traits = new Traits().putUserId("foo").putEmail("bar").putName("baz");
+    integration.identify(new IdentifyPayloadBuilder().traits(traits).build());
     verifyStatic();
     Bugsnag.setUser("foo", "bar", "baz");
     verifyStatic();
@@ -84,5 +110,35 @@ public class BugsnagRobolectricTest extends IntegrationRobolectricExam {
     Bugsnag.addToTab("User", "email", "bar");
     verifyStatic();
     Bugsnag.addToTab("User", "name", "baz");
+  }
+
+  @Test @Override public void group() {
+    integration.group(new GroupPayloadBuilder().build());
+    verifyStatic();
+    verifyNoMoreInteractions(Bugsnag.class);
+  }
+
+  @Test @Override public void track() {
+    integration.track(new TrackPayloadBuilder().build());
+    verifyStatic();
+    verifyNoMoreInteractions(Bugsnag.class);
+  }
+
+  @Test @Override public void alias() {
+    integration.alias(new AliasPayloadBuilder().build());
+    verifyStatic();
+    verifyNoMoreInteractions(Bugsnag.class);
+  }
+
+  @Test @Override public void screen() {
+    integration.screen(new ScreenPayloadBuilder().build());
+    verifyStatic();
+    verifyNoMoreInteractions(Bugsnag.class);
+  }
+
+  @Test @Override public void flush() {
+    integration.flush();
+    verifyStatic();
+    verifyNoMoreInteractions(Bugsnag.class);
   }
 }
