72a1ba1207dcc904374cc2afd98f313da20e6b23
Prateek Srivastava <f2prateek@gmail.com>
2014-12-18 11:46:18 -0700

    Automatically deploy snapshots to Sonatype

---
+++ .buildscript/deploy_snapshot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Deploy to Sonatype's snapshot repo.
+#
+# Adapted from https://coderwall.com/p/9b_lfq,
+# http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
+# and https://github.com/square/javawriter
+
+SLUG="segmentio/analytics-android"
+JDK="oraclejdk8"
+BRANCH="master"
+
+if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
+  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
+elif [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
+  echo "Skipping snapshot deployment: wrong JDK. Expected '$JDK' but was '$TRAVIS_JDK_VERSION'."
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "Skipping snapshot deployment: was pull request."
+elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+  echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
+else
+  echo "Deploying snapshot..."
+  ./gradlew clean build uploadArchives -PNEXUS_USERNAME=$CI_DEPLOY_NEXUS_USERNAME -PNEXUS_PASSWORD=$CI_DEPLOY_NEXUS_PASSWORD
+
+fi
