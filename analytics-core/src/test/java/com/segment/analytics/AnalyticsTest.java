402e70ae457395a46b5c998dbcd78803faba898c
Prateek Srivastava <f2prateek@gmail.com>
2015-02-17 16:58:57 -0700

    Analytics tests. Closes #169

--- analytics-core/src/test/java/com/segment/analytics/AnalyticsTest.java
+++ analytics-core/src/test/java/com/segment/analytics/AnalyticsTest.java
@@ -3,6 +3,10 @@ package com.segment.analytics;
 import android.Manifest;
 import android.app.Application;
 import com.segment.analytics.internal.model.payloads.BasePayload;
+import java.util.LinkedHashMap;
+import org.assertj.core.data.MapEntry;
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,9 +22,11 @@ import static com.segment.analytics.Analytics.BundledIntegration.MIXPANEL;
 import static com.segment.analytics.Analytics.LogLevel.NONE;
 import static com.segment.analytics.IntegrationManager.ActivityLifecyclePayload;
 import static com.segment.analytics.TestUtils.mockApplication;
+import static com.segment.analytics.internal.Utils.isNullOrEmpty;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -35,7 +41,8 @@ public class AnalyticsTest {
   @Mock SegmentDispatcher segmentDispatcher;
   @Mock Stats stats;
   @Mock Traits.Cache traitsCache;
-  @Mock AnalyticsContext analyticsContext;
+  Traits traits;
+  AnalyticsContext analyticsContext;
   @Mock Options defaultOptions;
 
   private Analytics analytics;
@@ -47,27 +54,70 @@ public class AnalyticsTest {
 
   @Before public void setUp() {
     initMocks(this);
-    grantPermission(Robolectric.application, Manifest.permission.INTERNET);
     application = mockApplication();
-    Traits traits = new Traits();
+    traits = Traits.create();
     when(traitsCache.get()).thenReturn(traits);
+    analyticsContext = new AnalyticsContext(new LinkedHashMap<String, Object>());
+    analyticsContext.setTraits(traits);
     analytics =
         new Analytics(application, integrationManager, segmentDispatcher, stats, traitsCache,
             analyticsContext, defaultOptions, NONE);
 
-    verify(application) //
-        .registerActivityLifecycleCallbacks(any(Application.ActivityLifecycleCallbacks.class));
+    // Used by singleton tests
+    grantPermission(Robolectric.application, Manifest.permission.INTERNET);
   }
 
   @After public void tearDown() {
     assertThat(ShadowLog.getLogs()).isEmpty();
   }
 
-  @Test public void logoutClearsTraitsAndUpdatesContext() {
-    analytics.logout();
-    verify(traitsCache).delete();
-    verify(traitsCache).set(any(Traits.class));
-    verify(analyticsContext).setTraits(traitsCache.get());
+  @Test public void registersForCallbacks() {
+    verify(application) //
+        .registerActivityLifecycleCallbacks(any(Application.ActivityLifecycleCallbacks.class));
+  }
+
+  @Test public void identify() {
+    try {
+      analytics.identify(null, null, null);
+    } catch (IllegalArgumentException e) {
+      assertThat(e).hasMessage("Either userId or some traits must be provided.");
+    }
+  }
+
+  @Test public void identifyUpdatesCache() {
+    analytics.identify("foo", new Traits().putValue("bar", "qaz"), null);
+
+    assertThat(traits).contains(MapEntry.entry("userId", "foo"))
+        .contains(MapEntry.entry("bar", "qaz"));
+    assertThat(analyticsContext.traits()).contains(MapEntry.entry("userId", "foo"))
+        .contains(MapEntry.entry("bar", "qaz"));
+    verify(traitsCache).set(traits);
+    verify(segmentDispatcher).dispatchEnqueue(argThat(new TypeSafeMatcher<BasePayload>() {
+      @Override protected boolean matchesSafely(BasePayload item) {
+        // Exercises a bug where payloads didn't pick up userId in identify correctly.
+        // https://github.com/segmentio/analytics-android/issues/169
+        return item.userId().equals("foo");
+      }
+
+      @Override public void describeTo(Description description) {
+      }
+    }));
+  }
+
+  @Test public void group() throws Exception {
+    try {
+      analytics.group(null);
+      fail("null groupId should throw exception");
+    } catch (IllegalArgumentException expected) {
+      assertThat(expected).hasMessage("groupId must not be null or empty.");
+    }
+
+    try {
+      analytics.group("");
+      fail("empty groupId and name should throw exception");
+    } catch (IllegalArgumentException expected) {
+      assertThat(expected).hasMessage("groupId must not be null or empty.");
+    }
   }
 
   @Test public void track() {
@@ -80,23 +130,6 @@ public class AnalyticsTest {
       analytics.track("   ");
     } catch (IllegalArgumentException e) {
       assertThat(e).hasMessage("event must not be null or empty.");
-    }
-  }
-
-  @Test public void alias() throws Exception {
-    try {
-      analytics.alias(null);
-      fail("null previous id should throw error");
-    } catch (IllegalArgumentException expected) {
-      assertThat(expected).hasMessage("previousId must not be null or empty.");
-    }
-
-    analytics.traitsCache.get().clear();
-    try {
-      analytics.alias("foo");
-      fail("null user id should throw error");
-    } catch (IllegalStateException expected) {
-      assertThat(expected).hasMessage("user must be identified with a userId before aliasing.");
     }
   }
 
@@ -116,39 +149,65 @@ public class AnalyticsTest {
     }
   }
 
-  @Test public void group() throws Exception {
+  @Test public void alias() throws Exception {
     try {
-      analytics.group(null);
-      fail("null groupId should throw exception");
+      analytics.alias(null);
+      fail("null previous id should throw error");
     } catch (IllegalArgumentException expected) {
-      assertThat(expected).hasMessage("groupId must not be null or empty.");
+      assertThat(expected).hasMessage("previousId must not be null or empty.");
     }
 
     try {
-      analytics.group("");
-      fail("empty groupId and name should throw exception");
-    } catch (IllegalArgumentException expected) {
-      assertThat(expected).hasMessage("groupId must not be null or empty.");
+      analytics.alias("foo");
+      fail("alias without identifying should throw");
+    } catch (IllegalStateException expected) {
+      assertThat(expected).hasMessage("user must be identified with a userId before aliasing.");
     }
   }
 
   @Test public void submitInvokesDispatch() {
     BasePayload payload = mock(BasePayload.class);
+
     analytics.submit(payload);
+
     verify(integrationManager).dispatchOperation(payload);
     verify(segmentDispatcher).dispatchEnqueue(payload);
+  }
+
+  @Test public void flushInvokesDispatch() throws Exception {
+    analytics.flush();
+
+    verify(segmentDispatcher).dispatchFlush(0);
+    verify(integrationManager).dispatchFlush();
+  }
+
+  @Test public void getSnapshot() throws Exception {
+    analytics.getSnapshot();
+
+    verify(stats).createSnapshot();
+  }
+
+  @Test public void logoutClearsTraitsAndUpdatesContext() {
+    analyticsContext.setTraits(new Traits().putAge(20).putAvatar("bar"));
+
+    analytics.logout();
+
+    verify(traitsCache).delete();
+    verify(traitsCache).set(argThat(new TypeSafeMatcher<Traits>() {
+      @Override protected boolean matchesSafely(Traits traits) {
+        return !isNullOrEmpty(traits.anonymousId());
+      }
+
+      @Override public void describeTo(Description description) {
+      }
+    }));
+    assertThat(analyticsContext.traits()).hasSize(1).containsKey("anonymousId");
   }
 
   @Test public void submitLifecycleInvokesDispatch() {
     ActivityLifecyclePayload payload = mock(ActivityLifecyclePayload.class);
     analytics.submit(payload);
     verify(integrationManager).dispatchOperation(payload);
-  }
-
-  @Test public void flushInvokesDispatch() throws Exception {
-    analytics.flush();
-    verify(segmentDispatcher).dispatchFlush(0);
-    verify(integrationManager).dispatchFlush();
   }
 
   @Test public void nullIntegrationManagerIsIgnored() throws Exception {
