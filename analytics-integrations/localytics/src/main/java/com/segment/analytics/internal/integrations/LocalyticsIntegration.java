66a94c690d6a968a882a13c5395aef552542cefb
Prateek Srivastava <f2prateek@gmail.com>
2015-03-03 14:34:14 -0700

    This is a combination of 2 commits.
    Update Localytics Integration

--- analytics-integrations/localytics/src/main/java/com/segment/analytics/internal/integrations/LocalyticsIntegration.java
+++ analytics-integrations/localytics/src/main/java/com/segment/analytics/internal/integrations/LocalyticsIntegration.java
@@ -1,11 +1,12 @@
 package com.segment.analytics.internal.integrations;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
-import android.os.Bundle;
-import com.localytics.android.LocalyticsAmpSession;
+import android.content.Intent;
+import android.location.Location;
+import com.localytics.android.Localytics;
 import com.segment.analytics.Analytics;
+import com.segment.analytics.AnalyticsContext;
 import com.segment.analytics.Traits;
 import com.segment.analytics.ValueMap;
 import com.segment.analytics.internal.AbstractIntegration;
@@ -16,7 +17,6 @@ import java.util.Map;
 
 import static com.segment.analytics.Analytics.LogLevel.INFO;
 import static com.segment.analytics.Analytics.LogLevel.VERBOSE;
-import static com.segment.analytics.internal.Utils.hasPermission;
 import static com.segment.analytics.internal.Utils.isNullOrEmpty;
 import static com.segment.analytics.internal.Utils.isOnClassPath;
 
@@ -28,87 +28,110 @@ import static com.segment.analytics.internal.Utils.isOnClassPath;
  * @see <a href="https://segment.com/docs/integrations/localytics/">Localytics Integration</a>
  * @see <a href="http://www.localytics.com/docs/android-integration/">Localytics Android SDK</a>
  */
-public class LocalyticsIntegration extends AbstractIntegration<LocalyticsAmpSession> {
+public class LocalyticsIntegration extends AbstractIntegration<Void> {
   static final String LOCALYTICS_KEY = "Localytics";
-  LocalyticsAmpSession session;
   boolean hasSupportLibOnClassPath;
 
   @Override public void initialize(Context context, ValueMap settings, Analytics.LogLevel logLevel)
       throws IllegalStateException {
-    if (!hasPermission(context, Manifest.permission.WAKE_LOCK)) {
-      throw new IllegalStateException("localytics requires the wake lock permission");
-    }
-    session = new LocalyticsAmpSession(context, settings.getString("appKey"));
-    LocalyticsAmpSession.setLoggingEnabled(logLevel == INFO || logLevel == VERBOSE);
+    Localytics.integrate(context, settings.getString("appKey"));
+    Localytics.setLoggingEnabled(logLevel == INFO || logLevel == VERBOSE);
     hasSupportLibOnClassPath = isOnClassPath("android.support.v4.app.FragmentActivity");
-  }
-
-  @Override public LocalyticsAmpSession getUnderlyingInstance() {
-    return session;
   }
 
   @Override public String key() {
     return LOCALYTICS_KEY;
   }
 
-  @Override public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-    super.onActivityCreated(activity, savedInstanceState);
-    session.open();
-    session.upload();
-  }
-
   @Override public void onActivityResumed(Activity activity) {
     super.onActivityResumed(activity);
-    session.open();
-    session.upload();
+
+    Localytics.openSession();
+    Localytics.upload();
 
     if (hasSupportLibOnClassPath) {
       if (activity instanceof android.support.v4.app.FragmentActivity) {
-        session.attach((android.support.v4.app.FragmentActivity) activity);
+        Localytics.setInAppMessageDisplayActivity(
+            (android.support.v4.app.FragmentActivity) activity);
       }
+    }
+
+    Intent intent = activity.getIntent();
+    if (intent != null) {
+      Localytics.handleTestMode(intent);
     }
   }
 
   @Override public void onActivityPaused(Activity activity) {
     super.onActivityPaused(activity);
+
     if (hasSupportLibOnClassPath) {
       if (activity instanceof android.support.v4.app.FragmentActivity) {
-        session.detach();
+        Localytics.dismissCurrentInAppMessage();
+        Localytics.clearInAppMessageDisplayActivity();
       }
     }
-    session.close();
-    session.upload();
+
+    Localytics.closeSession();
+    Localytics.upload();
   }
 
   @Override public void flush() {
     super.flush();
-    session.upload();
+
+    Localytics.upload();
   }
 
   @Override public void identify(IdentifyPayload identify) {
     super.identify(identify);
+
+    setContext(identify.context());
     Traits traits = identify.traits();
 
-    session.setCustomerId(identify.userId());
-
+    String userId = traits.userId();
+    if (!isNullOrEmpty(userId)) {
+      Localytics.setCustomerId(identify.userId());
+    }
     String email = traits.email();
-    if (!isNullOrEmpty(email)) session.setCustomerEmail(email);
-
+    if (!isNullOrEmpty(email)) {
+      Localytics.setIdentifier("email", email);
+    }
     String name = traits.name();
-    if (!isNullOrEmpty(name)) session.setCustomerName(name);
+    if (!isNullOrEmpty(name)) {
+      Localytics.setIdentifier("customer_name", name);
+    }
 
     for (Map.Entry<String, Object> entry : traits.entrySet()) {
-      session.setCustomerData(entry.getKey(), String.valueOf(entry.getValue()));
+      Localytics.setIdentifier(entry.getKey(), String.valueOf(entry.getValue()));
     }
   }
 
   @Override public void screen(ScreenPayload screen) {
     super.screen(screen);
-    session.tagScreen(screen.event());
+
+    setContext(screen.context());
+    Localytics.tagScreen(screen.event());
   }
 
   @Override public void track(TrackPayload track) {
     super.track(track);
-    session.tagEvent(track.event(), track.properties().toStringMap());
+
+    setContext(track.context());
+    Localytics.tagEvent(track.event(), track.properties().toStringMap());
+  }
+
+  private void setContext(AnalyticsContext context) {
+    if (isNullOrEmpty(context)) {
+      return;
+    }
+
+    AnalyticsContext.Location location = context.location();
+    if (location != null) {
+      Location androidLocation = new Location("Segment");
+      androidLocation.setLongitude(location.longitude());
+      androidLocation.setLatitude(location.latitude());
+      androidLocation.setSpeed((float) location.speed());
+      Localytics.setLocation(androidLocation);
+    }
   }
 }
