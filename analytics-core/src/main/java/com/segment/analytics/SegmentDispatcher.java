7d04fac7454392547cd463efb690781a69bf57aa
Frank Kelly <frank.kelly@here.com>
2015-05-20 16:02:36 -0400

    Go back to original code pre-fork

--- analytics-core/src/main/java/com/segment/analytics/SegmentDispatcher.java
+++ analytics-core/src/main/java/com/segment/analytics/SegmentDispatcher.java
@@ -251,12 +251,7 @@ class SegmentDispatcher extends AbstractIntegration {
     try {
       int payloadsUploaded = networkExecutor.submit(new Callable<Integer>() {
         @Override public Integer call() throws Exception {
-            try {
-                return upload();
-            } catch(IOException ioe) {
-                error(ioe, "Upload failed: " + ioe.getMessage());
-                return -1;
-            }
+            return upload();
         }
       }).get();
 
