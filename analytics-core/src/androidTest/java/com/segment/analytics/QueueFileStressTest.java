16a51faebefe7c7dcb737a66f3d6b35a6bdfb027
Frank Kelly <frank.kelly@here.com>
2015-05-20 10:35:18 -0400

    Stress test SegmentDispatcher and Thread test for Queue File

--- analytics-core/src/androidTest/java/com/segment/analytics/QueueFileStressTest.java
+++ analytics-core/src/androidTest/java/com/segment/analytics/QueueFileStressTest.java
@@ -28,24 +28,23 @@ public class QueueFileStressTest extends AndroidTestCase {
         // name (prefixes default names with a given prefix).
         context = new RenamingDelegatingContext(getContext(), "test_");
         Analytics.Builder builder = new Analytics.Builder(context, writeKey);
-        analyticsInstance = builder.build();
-
-        if(firstTimeThrough) {
-            sleepUntilSettingsLoaded();
-            firstTimeThrough = false;
-        }
+        analyticsInstance = builder.logLevel(Analytics.LogLevel.BASIC).build();
     }
 
     @Override
     public void tearDown() throws IOException {
         //Make sure any extra elements left over are flushed
-        analyticsInstance.flush();
+        //analyticsInstance.flush();
+        sleepUntilFlushTimerExpires();
     }
 
     public void testQueueFileUnderStress_track() {
 
         Analytics.Builder builder = new Analytics.Builder(context, writeKey);
-        analyticsInstance = builder.logLevel(Analytics.LogLevel.VERBOSE).build();
+        analyticsInstance = builder.logLevel(Analytics.LogLevel.VERBOSE).
+                flushInterval(300, TimeUnit.SECONDS).
+                flushQueueSize(SegmentDispatcher.MAX_QUEUE_SIZE*2).
+                build();
 
 
         int maxQueueSize = SegmentDispatcher.MAX_QUEUE_SIZE;
@@ -56,53 +55,28 @@ public class QueueFileStressTest extends AndroidTestCase {
         //WHEN we create enough events to cause a flush
         List<Integer> idList = new ArrayList<Integer>();
         //Avoid hitting the FLush Size
+        long id = System.currentTimeMillis();
         for (int i = 1; i <= veryLargeFlushSize - 1; i++) {
-            long id = System.currentTimeMillis();
-            analyticsInstance.track("FJK QueueFileStressTest - testQueueFileUnderStress_track - " + i + " of " + veryLargeFlushSize + " ID:[" + id + "]", null, null);
+            id++;
+            analyticsInstance.track("FJK QueueFileStressTest - testQueueFileUnderStress_track", null, null);
+            analyticsInstance.screen("FJK QueueFileStressTest - testQueueFileUnderStress_screen", null);
         }
+        analyticsInstance.flush();
 
-        //analyticsInstance.flush();
-
-        sleepUntilFlushTimerExpires();
-    }
-
-    public void testQueueFileUnderStress_screen() {
-
-        Analytics.Builder builder = new Analytics.Builder(context, writeKey);
-        analyticsInstance = builder.build();
-
-
-        int maxQueueSize = SegmentDispatcher.MAX_QUEUE_SIZE;
-        int aboveTheMax = 100;
-        int veryLargeFlushSize = maxQueueSize + aboveTheMax;
-
-
-        //WHEN we create enough events to cause a flush
-        List<Integer> idList = new ArrayList<Integer>();
-        //Avoid hitting the FLush Size
-        for (int i = 1; i <= veryLargeFlushSize - 1; i++) {
-            long id = System.currentTimeMillis();
-            analyticsInstance.screen("FJK QueueFileStressTest - testQueueFileUnderStress_screen - " + i + " of " + veryLargeFlushSize + " ID:[" + id + "]", null);
-        }
-
-        //analyticsInstance.flush();
-
-        sleepUntilFlushTimerExpires();
     }
 
     public void sleepUntilFlushTimerExpires() {
-        try {
-            Thread.sleep(Utils.DEFAULT_FLUSH_INTERVAL + 3000);
-        } catch (InterruptedException ignored) {
-
-        }
+       sleep(Utils.DEFAULT_FLUSH_INTERVAL + 3000);
     }
 
     public void sleepUntilSettingsLoaded() {
-        try {
-            Thread.sleep(60*1000);
-        } catch (InterruptedException ignored) {
+       sleep(60*1000);
+    }
 
+    public void sleep(long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException ignored) {
         }
     }
 }
