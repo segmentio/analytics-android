934d2157a02367e6c4ce95458a7991d6a67932be
vicv <victorvuc@gmail.com>
2015-05-20 16:11:55 -0700

    customdata changes

--- analytics-integrations/taplytics/src/main/java/com/segment/analytics/internal/integrations/TaplyticsIntegration.java
+++ analytics-integrations/taplytics/src/main/java/com/segment/analytics/internal/integrations/TaplyticsIntegration.java
@@ -8,10 +8,11 @@ import com.segment.analytics.internal.model.payloads.GroupPayload;
 import com.segment.analytics.internal.model.payloads.IdentifyPayload;
 import com.segment.analytics.internal.model.payloads.TrackPayload;
 import com.taplytics.sdk.Taplytics;
-import java.util.Map;
-import java.util.Set;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.Set;
 
 import static com.segment.analytics.internal.Utils.isNullOrEmpty;
 import static com.segment.analytics.internal.Utils.newSet;
@@ -25,74 +26,60 @@ import static com.segment.analytics.internal.Utils.newSet;
  * @see <a href="https://github.com/taplytics/Taplytics-Android-SDK">Taplytics Android SDK</a>
  */
 public class TaplyticsIntegration extends AbstractIntegration<Taplytics> {
-  static final String TAPLYTICS_KEY = "Taplytics";
+    static final String TAPLYTICS_KEY = "Taplytics";
 
-  private static final Set<String> KNOWN_TRAITS =
-      newSet("name", "email", "gender", "firstName", "lastName", "age");
+    private static final Set<String> KNOWN_TRAITS =
+            newSet("name", "email", "gender", "firstName", "lastName", "age");
 
-  @Override public void initialize(Analytics analytics, ValueMap settings)
-      throws IllegalStateException {
-    String apiKey = settings.getString("apiKey");
-    Taplytics.startTaplytics(analytics.getApplication(), apiKey);
-  }
-
-  @Override public String key() {
-    return TAPLYTICS_KEY;
-  }
-
-  @Override public void track(TrackPayload track) {
-    super.track(track);
-    JSONObject metaData = track.properties().toJsonObject();
-    Taplytics.logEvent(track.event(), track.properties().value(), metaData);
-  }
-
-  @Override public void identify(IdentifyPayload identify) {
-    super.identify(identify);
-
-    JSONObject userAttributes = new JSONObject();
-    Traits traits = identify.traits();
-    String userId = traits.userId();
-    if (!isNullOrEmpty(userId)) {
-      traits.remove("userId");
-      insert(userAttributes, "user_id", userId);
+    @Override
+    public void initialize(Analytics analytics, ValueMap settings)
+            throws IllegalStateException {
+        String apiKey = settings.getString("apiKey");
+        Taplytics.startTaplytics(analytics.getApplication(), apiKey);
     }
 
-    JSONObject customData = new JSONObject();
-    for (Map.Entry<String, Object> entry : traits.entrySet()) {
-      String key = entry.getKey();
-      Object value = entry.getValue();
-      if (KNOWN_TRAITS.contains(key)) {
-        insert(userAttributes, key, value);
-      } else {
-        insert(customData, key, value);
-      }
+    @Override
+    public String key() {
+        return TAPLYTICS_KEY;
     }
 
-    if (customData.length() > 0) {
-      insert(userAttributes, "customData", customData);
+    @Override
+    public void track(TrackPayload track) {
+        super.track(track);
+        JSONObject metaData = track.properties().toJsonObject();
+        Taplytics.logEvent(track.event(), track.properties().value(), metaData);
     }
 
-    Taplytics.setUserAttributes(userAttributes);
-  }
+    @Override
+    public void identify(IdentifyPayload identify) {
+        super.identify(identify);
 
-  static void insert(JSONObject target, String key, Object value) {
-    try {
-      target.put(key, value);
-    } catch (JSONException ignored) {
+        JSONObject userAttributes = identify.traits().toJsonObject();
+        Traits traits = identify.traits();
+        String userId = traits.userId();
+        if (!isNullOrEmpty(userId)) {
+            userAttributes.remove("userId");
+            insert(userAttributes, "user_id", userId);
+        }
+        Taplytics.setUserAttributes(userAttributes);
     }
-  }
 
-  @Override public void group(GroupPayload group) {
-    super.group(group);
+    static void insert(JSONObject target, String key, Object value) {
+        try {
+            target.put(key, value);
+        } catch (JSONException ignored) {
+        }
+    }
 
-    JSONObject customData = group.traits().toJsonObject();
-    JSONObject userAttributes = new JSONObject();
-    insert(userAttributes, "customData", customData);
-    Taplytics.setUserAttributes(userAttributes);
-  }
+    @Override
+    public void group(GroupPayload group) {
+        super.group(group);
+        Taplytics.setUserAttributes(group.traits().toJsonObject());
+    }
 
-  @Override public void reset() {
-    super.reset();
-    Taplytics.resetAppUser(null);
-  }
+    @Override
+    public void reset() {
+        super.reset();
+        Taplytics.resetAppUser(null);
+    }
 }
--- analytics-integrations/taplytics/src/test/java/com/segment/analytics/internal/integrations/TaplyticsTest.java
+++ analytics-integrations/taplytics/src/test/java/com/segment/analytics/internal/integrations/TaplyticsTest.java
@@ -3,6 +3,7 @@ package com.segment.analytics.internal.integrations;
 import android.app.Activity;
 import android.app.Application;
 import android.os.Bundle;
+
 import com.segment.analytics.Analytics;
 import com.segment.analytics.IntegrationTestRule;
 import com.segment.analytics.Properties;
@@ -15,6 +16,7 @@ import com.segment.analytics.internal.model.payloads.util.IdentifyPayloadBuilder
 import com.segment.analytics.internal.model.payloads.util.ScreenPayloadBuilder;
 import com.segment.analytics.internal.model.payloads.util.TrackPayloadBuilder;
 import com.taplytics.sdk.Taplytics;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
@@ -40,166 +42,183 @@ import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, emulateSdk = 18, manifest = Config.NONE)
-@PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*", "org.json.*" })
+@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*", "org.json.*"})
 @PrepareForTest(Taplytics.class)
 public class TaplyticsTest {
-  TaplyticsIntegration integration;
-  @Rule public PowerMockRule rule = new PowerMockRule();
-  @Rule public IntegrationTestRule integrationTestRule = new IntegrationTestRule();
-  @Mock Application context;
-  @Mock Analytics analytics;
+    TaplyticsIntegration integration;
+    @Rule
+    public PowerMockRule rule = new PowerMockRule();
+    @Rule
+    public IntegrationTestRule integrationTestRule = new IntegrationTestRule();
+    @Mock
+    Application context;
+    @Mock
+    Analytics analytics;
 
-  @Before public void setUp() {
-    initMocks(this);
-    PowerMockito.mockStatic(Taplytics.class);
-    integration = new TaplyticsIntegration();
-  }
+    @Before
+    public void setUp() {
+        initMocks(this);
+        PowerMockito.mockStatic(Taplytics.class);
+        integration = new TaplyticsIntegration();
+    }
 
-  @Test public void initialize() {
-    when(analytics.getApplication()).thenReturn(context);
-    integration.initialize(analytics, new ValueMap().putValue("apiKey", "foo"));
-    verifyStatic();
-    Taplytics.startTaplytics(eq(context), eq("foo"));
-    verifyNoMoreInteractions(Taplytics.class);
-  }
+    @Test
+    public void initialize() {
+        when(analytics.getApplication()).thenReturn(context);
+        integration.initialize(analytics, new ValueMap().putValue("apiKey", "foo"));
+        verifyStatic();
+        Taplytics.startTaplytics(eq(context), eq("foo"));
+        verifyNoMoreInteractions(Taplytics.class);
+    }
 
-  @Test public void track() {
-    integration.track(new TrackPayloadBuilder().event("foo").build());
+    @Test
+    public void track() {
+        integration.track(new TrackPayloadBuilder().event("foo").build());
 
-    verifyStatic();
-    Taplytics.logEvent(eq("foo"), eq(0.0), jsonEq(new JSONObject()));
-  }
+        verifyStatic();
+        Taplytics.logEvent(eq("foo"), eq(0.0), jsonEq(new JSONObject()));
+    }
 
-  @Test public void trackWithPropertiesAndValue() throws JSONException {
-    integration.track(new TrackPayloadBuilder().event("foo")
-        .properties(new Properties().putReferrer("bar").putValue(20))
-        .build());
+    @Test
+    public void trackWithPropertiesAndValue() throws JSONException {
+        integration.track(new TrackPayloadBuilder().event("foo")
+                .properties(new Properties().putReferrer("bar").putValue(20))
+                .build());
 
-    JSONObject metadata = new JSONObject();
-    metadata.put("referrer", "bar");
-    metadata.put("value", 20.0);
-    verifyStatic();
-    Taplytics.logEvent(eq("foo"), eq(20.0), jsonEq(metadata));
-  }
+        JSONObject metadata = new JSONObject();
+        metadata.put("referrer", "bar");
+        metadata.put("value", 20.0);
+        verifyStatic();
+        Taplytics.logEvent(eq("foo"), eq(20.0), jsonEq(metadata));
+    }
 
-  @Test public void identify() throws JSONException {
-    Traits traits = createTraits("foo");
-    integration.identify(new IdentifyPayloadBuilder().traits(traits).build());
+    @Test
+    public void identify() throws JSONException {
+        Traits traits = createTraits("foo");
+        integration.identify(new IdentifyPayloadBuilder().traits(traits).build());
 
-    JSONObject attributes = new JSONObject();
-    attributes.put("user_id", "foo");
+        JSONObject attributes = new JSONObject();
+        attributes.put("user_id", "foo");
 
-    verifyStatic();
-    Taplytics.setUserAttributes(jsonEq(attributes));
-    verifyNoMoreInteractions(Taplytics.class);
-  }
+        verifyStatic();
+        Taplytics.setUserAttributes(jsonEq(attributes));
+        verifyNoMoreInteractions(Taplytics.class);
+    }
 
-  @Test public void identifyWithTraits() throws JSONException {
-    Traits traits = createTraits("foo") //
-        .putName("bar")
-        .putEmail("baz")
-        .putGender("qux")
-        .putFirstName("foobar")
-        .putLastName("bazqux")
-        .putAge(30)
-        .putValue("custom", "foobarbazqux");
+    @Test
+    public void identifyWithTraits() throws JSONException {
+        Traits traits = createTraits("foo") //
+                .putName("bar")
+                .putEmail("baz")
+                .putGender("qux")
+                .putFirstName("foobar")
+                .putLastName("bazqux")
+                .putAge(30)
+                .putValue("custom", "foobarbazqux");
 
-    integration.identify(new IdentifyPayloadBuilder().traits(traits).build());
+        integration.identify(new IdentifyPayloadBuilder().traits(traits).build());
 
-    JSONObject userAttributes = new JSONObject();
-    userAttributes.put("user_id", "foo");
-    userAttributes.put("name", "bar");
-    userAttributes.put("email", "baz");
-    userAttributes.put("gender", "qux");
-    userAttributes.put("firstName", "foobar");
-    userAttributes.put("lastName", "bazqux");
-    userAttributes.put("age", 30);
-    JSONObject customData = new JSONObject();
-    customData.put("custom", "foobarbazqux");
-    userAttributes.put("customData", customData);
+        JSONObject userAttributes = new JSONObject();
+        userAttributes.put("user_id", "foo");
+        userAttributes.put("name", "bar");
+        userAttributes.put("email", "baz");
+        userAttributes.put("gender", "qux");
+        userAttributes.put("firstName", "foobar");
+        userAttributes.put("lastName", "bazqux");
+        userAttributes.put("age", 30);
+        userAttributes.put("custom", "foobarbazqux");
+        verifyStatic();
+        Taplytics.setUserAttributes(jsonEq(userAttributes));
+        verifyNoMoreInteractions(Taplytics.class);
+    }
 
-    verifyStatic();
-    Taplytics.setUserAttributes(jsonEq(userAttributes));
-    verifyNoMoreInteractions(Taplytics.class);
-  }
+    @Test
+    public void group() throws JSONException {
+        Traits traits = new Traits().putName("foo");
+        integration.group(new GroupPayloadBuilder().groupTraits(traits).build());
 
-  @Test public void group() throws JSONException {
-    Traits traits = new Traits().putName("foo");
-    integration.group(new GroupPayloadBuilder().groupTraits(traits).build());
+        JSONObject userAttributes = new JSONObject();
+        userAttributes.put("name", "foo");
 
-    JSONObject userAttributes = new JSONObject();
-    JSONObject metaData = new JSONObject();
-    metaData.put("name", "foo");
-    userAttributes.put("customData", metaData);
+        verifyStatic();
+        Taplytics.setUserAttributes(jsonEq(userAttributes));
+        verifyNoMoreInteractions(Taplytics.class);
+    }
 
-    verifyStatic();
-    Taplytics.setUserAttributes(jsonEq(userAttributes));
-    verifyNoMoreInteractions(Taplytics.class);
-  }
+    @Test
+    public void reset() {
+        integration.reset();
 
-  @Test public void reset() {
-    integration.reset();
+        verifyStatic();
+        Taplytics.resetAppUser(null);
+    }
 
-    verifyStatic();
-    Taplytics.resetAppUser(null);
-  }
+    @Test
+    public void activityCreate() {
+        Activity activity = mock(Activity.class);
+        Bundle bundle = mock(Bundle.class);
+        integration.onActivityCreated(activity, bundle);
+        verifyNoMoreInteractions(Taplytics.class);
+    }
 
-  @Test public void activityCreate() {
-    Activity activity = mock(Activity.class);
-    Bundle bundle = mock(Bundle.class);
-    integration.onActivityCreated(activity, bundle);
-    verifyNoMoreInteractions(Taplytics.class);
-  }
+    @Test
+    public void activityStart() {
+        Activity activity = mock(Activity.class);
+        integration.onActivityStarted(activity);
+        verifyNoMoreInteractions(Taplytics.class);
+    }
 
-  @Test public void activityStart() {
-    Activity activity = mock(Activity.class);
-    integration.onActivityStarted(activity);
-    verifyNoMoreInteractions(Taplytics.class);
-  }
+    @Test
+    public void activityResume() {
+        Activity activity = mock(Activity.class);
+        integration.onActivityResumed(activity);
+        verifyNoMoreInteractions(Taplytics.class);
+    }
 
-  @Test public void activityResume() {
-    Activity activity = mock(Activity.class);
-    integration.onActivityResumed(activity);
-    verifyNoMoreInteractions(Taplytics.class);
-  }
+    @Test
+    public void activityPause() {
+        Activity activity = mock(Activity.class);
+        integration.onActivityPaused(activity);
+        verifyNoMoreInteractions(Taplytics.class);
+    }
 
-  @Test public void activityPause() {
-    Activity activity = mock(Activity.class);
-    integration.onActivityPaused(activity);
-    verifyNoMoreInteractions(Taplytics.class);
-  }
+    @Test
+    public void activityStop() {
+        Activity activity = mock(Activity.class);
+        integration.onActivityStopped(activity);
+        verifyNoMoreInteractions(Taplytics.class);
+    }
 
-  @Test public void activityStop() {
-    Activity activity = mock(Activity.class);
-    integration.onActivityStopped(activity);
-    verifyNoMoreInteractions(Taplytics.class);
-  }
+    @Test
+    public void activitySaveInstance() {
+        Activity activity = mock(Activity.class);
+        Bundle bundle = mock(Bundle.class);
+        integration.onActivitySaveInstanceState(activity, bundle);
+        verifyNoMoreInteractions(Taplytics.class);
+    }
 
-  @Test public void activitySaveInstance() {
-    Activity activity = mock(Activity.class);
-    Bundle bundle = mock(Bundle.class);
-    integration.onActivitySaveInstanceState(activity, bundle);
-    verifyNoMoreInteractions(Taplytics.class);
-  }
+    @Test
+    public void activityDestroy() {
+        Activity activity = mock(Activity.class);
+        integration.onActivityDestroyed(activity);
+        verifyNoMoreInteractions(Taplytics.class);
+    }
 
-  @Test public void activityDestroy() {
-    Activity activity = mock(Activity.class);
-    integration.onActivityDestroyed(activity);
-    verifyNoMoreInteractions(Taplytics.class);
-  }
+    @Test
+    public void screen() {
+        integration.screen(new ScreenPayloadBuilder().name("somePage").build());
+        verifyNoMoreInteractions(Taplytics.class);
+    }
 
-  @Test public void screen() {
-    integration.screen(new ScreenPayloadBuilder().name("somePage").build());
-    verifyNoMoreInteractions(Taplytics.class);
-  }
+    @Test
+    public void flush() {
+        integration.flush();
+        verifyNoMoreInteractions(Taplytics.class);
+    }
 
-  @Test public void flush() {
-    integration.flush();
-    verifyNoMoreInteractions(Taplytics.class);
-  }
-
-  @Test public void alias() {
-    integration.alias(new AliasPayloadBuilder().build());
-    verifyNoMoreInteractions(Taplytics.class);
-  }
+    @Test
+    public void alias() {
+        integration.alias(new AliasPayloadBuilder().build());
+        verifyNoMoreInteractions(Taplytics.class);
+    }
 }
