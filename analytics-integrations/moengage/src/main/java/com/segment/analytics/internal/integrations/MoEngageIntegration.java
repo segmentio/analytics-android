4a6fa4cb7c7ea59c9874a4befee192faa0ef4130
Atrix1987 <iwizard.abhi@gmail.com>
2015-05-29 00:59:12 +0530

    Review comments from Segment Team

--- analytics-integrations/moengage/src/main/java/com/segment/analytics/internal/integrations/MoEngageIntegration.java
+++ analytics-integrations/moengage/src/main/java/com/segment/analytics/internal/integrations/MoEngageIntegration.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-
 import com.moe.pushlibrary.MoEHelper;
 import com.moe.pushlibrary.utils.MoEHelperConstants;
 import com.segment.analytics.Analytics;
@@ -14,7 +13,6 @@ import com.segment.analytics.ValueMap;
 import com.segment.analytics.internal.AbstractIntegration;
 import com.segment.analytics.internal.model.payloads.IdentifyPayload;
 import com.segment.analytics.internal.model.payloads.TrackPayload;
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
@@ -30,144 +28,127 @@ import static com.segment.analytics.internal.Utils.hasPermission;
  * @see <a href="https://segment.com/docs/integrations/moengage/">MoEngage Integration</a>
  * @see <a href="http://docs.moengage.com/en/latest/android.html">MoEngage Android SDK</a>
  */
-public class MoEngageIntegration extends AbstractIntegration<Void> {
+public class MoEngageIntegration extends AbstractIntegration<MoEHelper> {
 
-    private static final String KEY_MOENGAGE = "MoEngage";
+  private static final String KEY_MOENGAGE = "MoEngage";
 
-    private boolean setUserId = false;
-    MoEHelper mHelper;
+  MoEHelper helper;
 
-    private static final String ANONYMOUS_ID_KEY = "anonymousId";
-    private static final String EMAIL_KEY = "email";
-    private static final String USER_ID_KEY = "userId";
-    private static final String NAME_KEY = "name";
-    private static final String PHONE_KEY = "phone";
-    private static final String BIRTHDAY_KEY = "birthday";
-    private static final String FIRST_NAME_KEY = "firstName";
-    private static final String GENDER_KEY = "gender";
-    private static final String LAST_NAME_KEY = "lastName";
-    private static final String USER_ATTR_SEGMENT_AID = "USER_ATTRIBUTE_SEGMENT_ID";
+  private static final String ANONYMOUS_ID_KEY = "anonymousId";
+  private static final String EMAIL_KEY = "email";
+  private static final String USER_ID_KEY = "userId";
+  private static final String NAME_KEY = "name";
+  private static final String PHONE_KEY = "phone";
+  private static final String BIRTHDAY_KEY = "birthday";
+  private static final String FIRST_NAME_KEY = "firstName";
+  private static final String GENDER_KEY = "gender";
+  private static final String LAST_NAME_KEY = "lastName";
+  private static final String USER_ATTR_SEGMENT_AID = "USER_ATTRIBUTE_SEGMENT_ID";
 
-    private boolean hasLocationPermissions = false;
+  @Override public void initialize(Analytics analytics, ValueMap settings)
+      throws IllegalStateException {
+    Context context = analytics.getApplication();
+    if (!hasPermission(context, "com.google.android.c2dm.permission.RECEIVE")) {
+      throw new IllegalStateException(
+          "MoEngage requires com.google.android.c2dm.permission.RECEIVE permission");
+    }
+    Analytics.LogLevel logLevel = analytics.getLogLevel();
+    if (logLevel == Analytics.LogLevel.VERBOSE || logLevel == Analytics.LogLevel.INFO) {
+      MoEHelper.APP_DEBUG = true;
+    }
+  }
 
-    @Override
-    public void initialize(Analytics analytics, ValueMap settings) throws IllegalStateException {
-        Context context =  analytics.getApplication();
-        if (!hasPermission(context, "com.google.android.c2dm.permission.RECEIVE")) {
-            throw new IllegalStateException(
-                    "MoEngage requires com.google.android.c2dm.permission.RECEIVE permission");
+  @Override public String key() {
+    return KEY_MOENGAGE;
+  }
+
+  @Override public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+    super.onActivityCreated(activity, savedInstanceState);
+    helper = new MoEHelper(activity);
+    if (null != savedInstanceState) {
+      helper.onRestoreInstanceState(savedInstanceState);
+    }
+  }
+
+  @Override public void onActivityStarted(Activity activity) {
+    super.onActivityStarted(activity);
+    helper.onStart(activity);
+  }
+
+  @Override public void onActivityResumed(Activity activity) {
+    super.onActivityResumed(activity);
+    helper.onResume(activity);
+  }
+
+  @Override public void onActivityPaused(Activity activity) {
+    super.onActivityPaused(activity);
+    helper.onPause(activity);
+  }
+
+  @Override public void onActivityStopped(Activity activity) {
+    super.onActivityStopped(activity);
+    helper.onStop(activity);
+  }
+
+  @Override public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+    super.onActivitySaveInstanceState(activity, outState);
+    helper.onSaveInstanceState(outState);
+  }
+
+  @Override public void identify(IdentifyPayload identify) {
+    super.identify(identify);
+    Traits tr = identify.traits();
+    if (null != tr && !tr.isEmpty()) {
+      HashMap<String, Object> processedAttrs = new HashMap<String, Object>();
+      Set<String> keys = tr.keySet();
+      for (String key : keys) {
+        if (null != key) {
+          if (ANONYMOUS_ID_KEY.equals(key)) {
+            processedAttrs.put(USER_ATTR_SEGMENT_AID, tr.get(key));
+          } else if (EMAIL_KEY.equals(key)) {
+            processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_USER_EMAIL, tr.get(key));
+          } else if (USER_ID_KEY.equals(key)) {
+            processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_UNIQUE_ID, tr.get(key));
+          } else if (NAME_KEY.equals(key)) {
+            processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_USER_NAME, tr.get(key));
+          } else if (PHONE_KEY.equals(key)) {
+            processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_USER_MOBILE, tr.get(key));
+          } else if (FIRST_NAME_KEY.equals(key)) {
+            processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_USER_FIRST_NAME, tr.get(key));
+          } else if (LAST_NAME_KEY.equals(key)) {
+            processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_USER_LAST_NAME, tr.get(key));
+          } else if (GENDER_KEY.equals(key)) {
+            processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_USER_GENDER, tr.get(key));
+          } else if (BIRTHDAY_KEY.equals(key)) {
+            DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+            String birthDate = df.format(tr.birthday());
+            processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_USER_BDAY, birthDate);
+          } else {
+            processedAttrs.put(key, tr.get(key));
+          }
+        } else if (MoEHelper.APP_DEBUG) {
+          Log.e(MoEHelper.TAG, "TRAIT KEY CANNOT BE NULL");
         }
-        if ( hasPermission(context, "android.permission.ACCESS_FINE_LOCATION")) {
-            hasLocationPermissions = true;
-        }
-        Analytics.LogLevel logLevel = analytics.getLogLevel();
-        if( logLevel == Analytics.LogLevel.VERBOSE || logLevel == Analytics.LogLevel.INFO ){
-            MoEHelper.APP_DEBUG = true;
-        }
-        mHelper = MoEHelper.getInstance(context);
+      }
+      helper.setUserAttribute(processedAttrs);
     }
-
-    @Override
-    public String key() {
-        return KEY_MOENGAGE;
+    AnalyticsContext.Location location = identify.context().location();
+    if (location != null) {
+      helper.setUserLocation(location.latitude(), location.longitude());
     }
+  }
 
-    @Override
-    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-        super.onActivityCreated(activity, savedInstanceState);
-        mHelper = new MoEHelper(activity);
-        if( null != savedInstanceState ){
-            mHelper.onRestoreInstanceState(savedInstanceState);
-        }
-    }
+  @Override public void track(TrackPayload track) {
+    super.track(track);
+    helper.trackEvent(track.event(), track.properties().toStringMap());
+  }
 
-    @Override
-    public void onActivityStarted(Activity activity) {
-        super.onActivityStarted(activity);
-        mHelper.onStart(activity);
-    }
+  @Override public void reset() {
+    super.reset();
+    helper.logoutUser();
+  }
 
-    @Override
-    public void onActivityResumed(Activity activity) {
-        super.onActivityResumed(activity);
-        mHelper.onResume(activity);
-    }
-
-    @Override
-    public void onActivityPaused(Activity activity) {
-        super.onActivityPaused(activity);
-        mHelper.onPause(activity);
-    }
-
-    @Override
-    public void onActivityStopped(Activity activity) {
-        super.onActivityStopped(activity);
-        mHelper.onStop(activity);
-    }
-
-    @Override
-    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-        super.onActivitySaveInstanceState(activity, outState);
-        mHelper.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void identify(IdentifyPayload identify) {
-        super.identify(identify);
-        Traits tr = identify.traits();
-        if( null == tr || tr.isEmpty()){
-            return;
-        }
-        Set<String> keys = tr.keySet();
-        HashMap<String, Object> processedAttrs = new HashMap<String, Object>();
-        for(String key : keys){
-            if( null != key ){
-                if( ANONYMOUS_ID_KEY.equals(key)) {
-                    processedAttrs.put(USER_ATTR_SEGMENT_AID, tr.get(key));
-                } else if(EMAIL_KEY.equals(key)){
-                    processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_USER_EMAIL, tr.get(key));
-                } else if(USER_ID_KEY.equals(key)){
-                    processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_UNIQUE_ID, tr.get(key));
-                } else if(NAME_KEY.equals(key)){
-                    processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_USER_NAME, tr.get(key));
-                } else if(PHONE_KEY.equals(key)){
-                    processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_USER_MOBILE, tr.get(key));
-                } else if(FIRST_NAME_KEY.equals(key)){
-                    processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_USER_FIRST_NAME, tr.get(key));
-                } else if(LAST_NAME_KEY.equals(key)){
-                    processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_USER_LAST_NAME, tr.get(key));
-                } else if(GENDER_KEY.equals(key)) {
-                    processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_USER_GENDER, tr.get(key));
-                } else if(BIRTHDAY_KEY.equals(key)) {
-                    DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
-                    String birthDate = df.format(tr.birthday());
-                    processedAttrs.put(MoEHelperConstants.USER_ATTRIBUTE_USER_BDAY, birthDate);
-                } else {
-                    processedAttrs.put(key, tr.get(key));
-                }
-            }else if(MoEHelper.APP_DEBUG){
-                Log.e(MoEHelper.TAG, "TRAIT KEY CANNOT BE NULL");
-            }
-        }
-        if( hasLocationPermissions ){
-            AnalyticsContext.Location location = identify.context().location();
-            if (location != null) {
-                mHelper.setUserLocation( location.latitude(),  location.longitude());
-            }
-        }
-        mHelper.setUserAttribute(processedAttrs);
-    }
-
-    @Override
-    public void track(TrackPayload track) {
-        super.track(track);
-        mHelper.trackEvent(track.event(), track.properties().toStringMap());
-    }
-
-    @Override
-    public void reset() {
-        super.reset();
-        mHelper.logoutUser();
-    }
-
+  @Override public MoEHelper getUnderlyingInstance() {
+    return helper;
+  }
 }
--- analytics-integrations/moengage/src/test/java/com/segment/analytics/internal/integrations/MoEngageTest.java
+++ analytics-integrations/moengage/src/test/java/com/segment/analytics/internal/integrations/MoEngageTest.java
@@ -4,19 +4,23 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
-
 import com.moe.pushlibrary.MoEHelper;
+import com.moe.pushlibrary.utils.MoEHelperConstants;
 import com.segment.analytics.Analytics;
+import com.segment.analytics.AnalyticsContext;
 import com.segment.analytics.IntegrationTestRule;
+import com.segment.analytics.Properties;
 import com.segment.analytics.TestUtils;
 import com.segment.analytics.Traits;
+import com.segment.analytics.ValueMap;
 import com.segment.analytics.core.tests.BuildConfig;
 import com.segment.analytics.internal.model.payloads.util.AliasPayloadBuilder;
 import com.segment.analytics.internal.model.payloads.util.GroupPayloadBuilder;
 import com.segment.analytics.internal.model.payloads.util.IdentifyPayloadBuilder;
 import com.segment.analytics.internal.model.payloads.util.ScreenPayloadBuilder;
 import com.segment.analytics.internal.model.payloads.util.TrackPayloadBuilder;
-
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -28,114 +32,169 @@ import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
+import static com.segment.analytics.TestUtils.createContext;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.verifyNoMoreInteractions;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, emulateSdk = 18, manifest = Config.NONE)
-@PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*", "org.json.*"  })
-@PrepareForTest(MoEngageIntegration.class)
-public class MoEngageTest {
+@PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*", "org.json.*" })
+@PrepareForTest(MoEngageIntegration.class) public class MoEngageTest {
 
-    @Rule public PowerMockRule rule = new PowerMockRule();
-    @Rule public IntegrationTestRule integrationTestRule = new IntegrationTestRule();
+  @Rule public PowerMockRule rule = new PowerMockRule();
+  @Rule public IntegrationTestRule integrationTestRule = new IntegrationTestRule();
 
-    @Mock Application application;
-    @Mock Context context;
-    @Mock Analytics analytics;
-    MoEngageIntegration integration;
+  @Mock Application application;
+  @Mock Context context;
+  @Mock Analytics analytics;
+  @Mock MoEHelper moeHelper;
+  MoEngageIntegration integration;
 
-    @Before
-    public void setUp() {
-        initMocks(this);
-        integration = new MoEngageIntegration();
-        integration.mHelper = MoEHelper.getInstance(application);
+  @Before public void setUp() {
+    initMocks(this);
+    mockStatic(MoEHelper.class);
+    integration = new MoEngageIntegration();
+    integration.helper = moeHelper;
+
+  }
+
+  @Test public void initialize() {
+    when(analytics.getApplication()).thenReturn(application);
+
+    integration.initialize(analytics, new ValueMap());
+    Analytics.LogLevel logLevel = analytics.getLogLevel();
+    if (logLevel == Analytics.LogLevel.VERBOSE || logLevel == Analytics.LogLevel.INFO) {
+      assertThat(MoEHelper.APP_DEBUG).isTrue();
+    }else{
+      assertThat(MoEHelper.APP_DEBUG).isFalse();
     }
+    verifyNoMoreInteractions(MoEHelper.class);
+    verifyNoMoreInteractions(moeHelper);
+  }
 
-    @Test public void initialize(){
-        //nothing to be done
-    }
+  @Test public void activityCreate() {
+    Activity activity = mock(Activity.class);
+    Bundle bundle = mock(Bundle.class);
+    integration.onActivityCreated(activity, bundle);
+    verifyNoMoreInteractions(MoEHelper.class);
+    verifyNoMoreInteractions(moeHelper);
+  }
 
-    @Test public void activityCreate() {
-        Activity activity = mock(Activity.class);
-        Bundle bundle = mock(Bundle.class);
-        integration.onActivityCreated(activity, bundle);
-        verifyNoMoreInteractions(MoEHelper.class);
-    }
+  @Test public void activityStart() {
+    Activity activity = mock(Activity.class);
+    integration.onActivityStarted(activity);
+    verify(moeHelper).onStart(activity);
+    verifyNoMoreInteractions(MoEHelper.class);
+    verifyNoMoreInteractions(moeHelper);
+  }
 
-    @Test public void activityStart() {
-        Activity activity = mock(Activity.class);
-        integration.onActivityStarted(activity);
-        verifyNoMoreInteractions(MoEHelper.class);
-    }
+  @Test public void activityResume() {
+    Activity activity = mock(Activity.class);
+    integration.onActivityResumed(activity);
+    verify(moeHelper).onResume(activity);
+    verifyNoMoreInteractions(MoEHelper.class);
+    verifyNoMoreInteractions(moeHelper);
+  }
 
-    @Test public void activityResume() {
-        Activity activity = mock(Activity.class);
-        integration.onActivityResumed(activity);
-        verifyNoMoreInteractions(MoEHelper.class);
-    }
+  @Test public void activityPause() {
+    Activity activity = mock(Activity.class);
+    integration.onActivityPaused(activity);
+    verify(moeHelper).onPause(activity);
+    verifyNoMoreInteractions(MoEHelper.class);
+    verifyNoMoreInteractions(moeHelper);
+  }
 
-    @Test public void activityPause() {
-        Activity activity = mock(Activity.class);
-        integration.onActivityPaused(activity);
-        verifyNoMoreInteractions(MoEHelper.class);
-    }
+  @Test public void activityStop() {
+    Activity activity = mock(Activity.class);
+    integration.onActivityStopped(activity);
+    verify(moeHelper).onStop(activity);
+    verifyNoMoreInteractions(MoEHelper.class);
+    verifyNoMoreInteractions(moeHelper);
+  }
 
-    @Test public void activityStop() {
-        Activity activity = mock(Activity.class);
-        integration.onActivityStopped(activity);
-        verifyNoMoreInteractions(MoEHelper.class);
-    }
+  @Test public void activityDestroy() {
+    Activity activity = mock(Activity.class);
+    integration.onActivityDestroyed(activity);
+    verifyNoMoreInteractions(MoEHelper.class);
+    verifyNoMoreInteractions(moeHelper);
+  }
 
-    @Test public void activityDestroy(){
-        //do nothing
-    }
+  @Test public void group() {
+    integration.group(new GroupPayloadBuilder().build());
+    verifyNoMoreInteractions(MoEHelper.class);
+    verifyNoMoreInteractions(moeHelper);
+  }
 
-    @Test public void group() {
-        integration.group(new GroupPayloadBuilder().build());
-        verifyNoMoreInteractions(MoEHelper.class);
-    }
+  @Test public void alias() {
+    integration.alias(new AliasPayloadBuilder().build());
+    verifyNoMoreInteractions(MoEHelper.class);
+    verifyNoMoreInteractions(moeHelper);
+  }
 
-    @Test public void alias() {
-        integration.alias(new AliasPayloadBuilder().build());
-        verifyNoMoreInteractions(MoEHelper.class);
-    }
-    @Test
-    public void activitySaveInstance() {
-        Activity activity = mock(Activity.class);
-        Bundle bundle = mock(Bundle.class);
-        integration.onActivitySaveInstanceState(activity, bundle);
-        verifyNoMoreInteractions(MoEHelper.class);
-    }
+  @Test public void activitySaveInstance() {
+    Activity activity = mock(Activity.class);
+    Bundle bundle = mock(Bundle.class);
+    integration.onActivitySaveInstanceState(activity, bundle);
+    verify(moeHelper).onSaveInstanceState(bundle);
+    verifyNoMoreInteractions(MoEHelper.class);
+    verifyNoMoreInteractions(moeHelper);
+  }
 
+  @Test public void track() {
 
-    @Test public void track() {
-        Activity activity = mock(Activity.class);
-        integration.track(new TrackPayloadBuilder().event("foo").build());
-        verifyNoMoreInteractions(MoEHelper.class);
-    }
+    TrackPayloadBuilder builder = new TrackPayloadBuilder();
+    builder.event("foo").properties(new Properties().putCurrency("INR").putPrice(2000));
+    integration.track(builder.build());
 
-    @Test public void screen() {
-        integration.screen(new ScreenPayloadBuilder().build());
-        verifyNoMoreInteractions(MoEHelper.class);
-    }
+    Map<String, String> eventProperties = new HashMap<>();
+    eventProperties.put("currency", "INR");
+    eventProperties.put("price", "2000.0");
+    verify(moeHelper).trackEvent("foo", eventProperties);
+    verifyNoMoreInteractions(MoEHelper.class);
+    verifyNoMoreInteractions(moeHelper);
+  }
 
-    @Test public void identify() {
-        Traits traits = TestUtils.createTraits("foo");
-        traits.putEmail("foo@bar.com");
-        integration.identify(new IdentifyPayloadBuilder().traits(traits).build());
-        verifyNoMoreInteractions(MoEHelper.class);
-    }
+  @Test public void screen() {
+    integration.screen(new ScreenPayloadBuilder().build());
+    verifyNoMoreInteractions(MoEHelper.class);
+    verifyNoMoreInteractions(moeHelper);
+  }
 
-    @Test public void flush() {
-        integration.flush();
-        verifyNoMoreInteractions(MoEHelper.class);
-    }
+  @Test public void identify() {
+    Traits traits = TestUtils.createTraits();
+    traits.putEmail("foo@bar.com");
+    HashMap<String, Object> traitMap = new HashMap<>();
+    traitMap.put(MoEHelperConstants.USER_ATTRIBUTE_USER_EMAIL, traits.email());
+    traitMap.put("USER_ATTRIBUTE_SEGMENT_ID", traits.anonymousId());
+    AnalyticsContext analyticsContext = createContext(traits).putLocation(
+        new AnalyticsContext.Location().putLatitude(20).putLongitude(20));
 
-    @Test public void reset() {
-        integration.reset();
-        verifyNoMoreInteractions(MoEHelper.class);
-    }
+    integration.identify(
+        new IdentifyPayloadBuilder().traits(traits).context(analyticsContext).build());
 
+    verify(moeHelper).setUserAttribute(traitMap);
+    verify(moeHelper).setUserLocation(20, 20);
+
+    verifyNoMoreInteractions(moeHelper);
+    verifyNoMoreInteractions(MoEHelper.class);
+
+  }
+
+  @Test public void flush() {
+    integration.flush();
+    verifyNoMoreInteractions(MoEHelper.class);
+    verifyNoMoreInteractions(moeHelper);
+  }
+
+  @Test public void reset() {
+    integration.reset();
+    verify(moeHelper).logoutUser();
+    verifyNoMoreInteractions(MoEHelper.class);
+    verifyNoMoreInteractions(moeHelper);
+  }
 }
