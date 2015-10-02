1ae0b8d6232de42762d43814d00e8686527987c9
Prateek Srivastava <f2prateek@gmail.com>
2015-10-02 15:03:22 -0700

    dont build everything

--- .buildscript/setup.sh
+++ .buildscript/setup.sh
@@ -9,6 +9,5 @@ DEPS="$ANDROID_HOME/installed-dependencies"
 if [ ! -e $DEPS ]; then
   cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
   echo y | android update sdk --no-ui --all --filter tools,platform-tools,build-tools-23.0.1,android-23,extra-google-m2repository,extra-google-google_play_services,extra-android-support,extra-android-m2repository &&
-  TERM=dumb ./gradlew build &&
   touch $DEPS
 fi
