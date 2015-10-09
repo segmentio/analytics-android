d598409c2ad1a92aef0d0697c9740f6cf46ba1f4
absidibe <ali@followanalytics.com>
2015-10-09 16:30:48 +0200

    Add test and fix checkstyle error

--- analytics-integrations/followanalytics/src/main/java/com/segment/analytics/internal/integrations/FollowanalyticsIntegration.java
+++ analytics-integrations/followanalytics/src/main/java/com/segment/analytics/internal/integrations/FollowanalyticsIntegration.java
@@ -13,32 +13,50 @@ import java.util.Map;
 import static com.segment.analytics.Analytics.LogLevel.INFO;
 import static com.segment.analytics.Analytics.LogLevel.VERBOSE;
 
-public class FollowanalyticsIntegration extends AbstractIntegration<Void> {
+public class FollowAnalyticsIntegration extends AbstractIntegration<Void> {
 
-
+    static final String FAID = "faid";
     static final String FOLLOW_ANALYTICS_KEY = "followanalytics";
-    private final int BG_SESSION_TIMEOUT = 120;
+    static final String KEY_COLLECT_LOCATION = "collectLocation";
+    static final String KEY_COLLECT_LOG = "collectLog";
+    static final String KEY_SESSION_TIMEOUT = "sessionTimeout";
+
+    private static final int SESSION_TIMEOUT_DEFAULT_VALUE = 120;
+
 
     @Override
     public void initialize(Analytics analytics, ValueMap settings) throws IllegalStateException {
-        String faid = settings.getString("faid");
-        if(faid ==null || faid.isEmpty()){
-            throw  new IllegalArgumentException("faid cannot be null");
+        String faid = settings.getString(FAID);
+        if (faid == null || faid.isEmpty()) {
+            throw new IllegalArgumentException(FAID + " cannot be null");
         }
         FollowApps.init(analytics.getApplication(), faid);
+
+        boolean canCollectLocationLog = settings.getBoolean(KEY_COLLECT_LOCATION, true);
+        FollowApps.setCollectLocationLogsAuthorization(canCollectLocationLog);
+        boolean canCollectLog = settings.getBoolean(KEY_COLLECT_LOG, true);
+        FollowApps.setCollectLogsAuthorization(canCollectLog);
+        int sessionTimeout = settings.getInt(KEY_SESSION_TIMEOUT, SESSION_TIMEOUT_DEFAULT_VALUE);
+        FollowApps.setMaximumBackgroundTimeWithinSession(sessionTimeout);
         Analytics.LogLevel logLevel = analytics.getLogLevel();
         FollowApps.setVerbose(logLevel == INFO || logLevel == VERBOSE);
         FollowApps.registerGcm();
-        FollowApps.setCollectLocationLogsAuthorization(settings.getBoolean("collectLocation", true));
-        FollowApps.setCollectLogsAuthorization(settings.getBoolean("collectLog", true));
-        FollowApps.setMaximumBackgroundTimeWithinSession(settings.getInt("sessionTimeout", BG_SESSION_TIMEOUT));
+
     }
 
     @Override
     public void identify(IdentifyPayload identify) {
         super.identify(identify);
-        FollowApps.setCurrentIdentifier(identify.userId());
-        for (Map.Entry<String, Object> entry : identify.traits().entrySet()) {
+        String userId = identify.userId();
+        if (userId == null || userId.isEmpty()) {
+            return;
+        }
+        FollowApps.setCurrentIdentifier(userId);
+        Map<String, String> attributes = identify.toStringMap();
+        if (attributes == null || attributes.isEmpty()) {
+            return;
+        }
+        for (Map.Entry<String, String> entry : attributes.entrySet()) {
             String key = entry.getKey();
             String value = entry.getValue().toString();
             FollowApps.setUserAttribute(key, value);
@@ -48,7 +66,10 @@ public class FollowanalyticsIntegration extends AbstractIntegration<Void> {
     @Override
     public void track(TrackPayload track) {
         super.track(track);
-        FollowApps.logEvent(track.event(),track.properties().toStringMap());
+        if (track == null || track.event() == null) {
+            return;
+        }
+        FollowApps.logEvent(track.event(), track.toStringMap());
     }
 
     @Override
