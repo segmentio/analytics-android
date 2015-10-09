a416a46ffe8bffa6ba392d9869f9058bf7c0dbe1
absidibe <ali@followanalytics.com>
2015-10-09 12:03:48 +0200

    setup follow analytics initialization

--- analytics-integrations/followanalytics/src/main/java/com/segment/analytics/internal/integrations/FollowanalyticsIntegration.java
+++ analytics-integrations/followanalytics/src/main/java/com/segment/analytics/internal/integrations/FollowanalyticsIntegration.java
@@ -1,19 +1,58 @@
 package com.segment.analytics.internal.integrations;
 
 
+import com.followapps.android.FollowApps;
 import com.segment.analytics.Analytics;
 import com.segment.analytics.ValueMap;
 import com.segment.analytics.internal.AbstractIntegration;
+import com.segment.analytics.internal.model.payloads.IdentifyPayload;
+import com.segment.analytics.internal.model.payloads.TrackPayload;
+
+import java.util.Map;
+
+import static com.segment.analytics.Analytics.LogLevel.INFO;
+import static com.segment.analytics.Analytics.LogLevel.VERBOSE;
 
 public class FollowanalyticsIntegration extends AbstractIntegration<Void> {
 
 
-    @Override public void initialize(Analytics analytics, ValueMap settings) throws IllegalStateException {
+    static final String FOLLOW_ANALYTICS_KEY = "followanalytics";
+    private final int BG_SESSION_TIMEOUT = 120;
 
+    @Override
+    public void initialize(Analytics analytics, ValueMap settings) throws IllegalStateException {
+        String faid = settings.getString("faid");
+        if(faid ==null || faid.isEmpty()){
+            throw  new IllegalArgumentException("faid cannot be null");
+        }
+        FollowApps.init(analytics.getApplication(), faid);
+        Analytics.LogLevel logLevel = analytics.getLogLevel();
+        FollowApps.setVerbose(logLevel == INFO || logLevel == VERBOSE);
+        FollowApps.registerGcm();
+        FollowApps.setCollectLocationLogsAuthorization(settings.getBoolean("collectLocation", true));
+        FollowApps.setCollectLogsAuthorization(settings.getBoolean("collectLog", true));
+        FollowApps.setMaximumBackgroundTimeWithinSession(settings.getInt("sessionTimeout", BG_SESSION_TIMEOUT));
+    }
+
+    @Override
+    public void identify(IdentifyPayload identify) {
+        super.identify(identify);
+        FollowApps.setCurrentIdentifier(identify.userId());
+        for (Map.Entry<String, Object> entry : identify.traits().entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue().toString();
+            FollowApps.setUserAttribute(key, value);
+        }
+    }
+
+    @Override
+    public void track(TrackPayload track) {
+        super.track(track);
+        FollowApps.logEvent(track.event(),track.properties().toStringMap());
     }
 
     @Override
     public String key() {
-        return null;
+        return FOLLOW_ANALYTICS_KEY;
     }
 }
