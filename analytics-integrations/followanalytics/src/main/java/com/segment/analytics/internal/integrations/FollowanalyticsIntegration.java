49e6a58f9c82ca9b216d7eb6ab30b6d7aa7632d2
absidibe <ali@followanalytics.com>
2015-10-09 10:26:56 +0200

    Create structure of integration

---
+++ analytics-integrations/followanalytics/src/main/java/com/segment/analytics/internal/integrations/FollowanalyticsIntegration.java
@@ -0,0 +1,19 @@
+package com.segment.analytics.internal.integrations;
+
+
+import com.segment.analytics.Analytics;
+import com.segment.analytics.ValueMap;
+import com.segment.analytics.internal.AbstractIntegration;
+
+public class FollowanalyticsIntegration extends AbstractIntegration<Void> {
+
+
+    @Override public void initialize(Analytics analytics, ValueMap settings) throws IllegalStateException {
+
+    }
+
+    @Override
+    public String key() {
+        return null;
+    }
+}
