dc98af81b88838168eddd406eb82ac6c40da1870
Prateek Srivastava <f2prateek@gmail.com>
2017-02-17 11:07:02 -0800

    update cartographer

--- analytics/src/main/java/com/segment/analytics/Cartographer.java
+++ analytics/src/main/java/com/segment/analytics/Cartographer.java
@@ -38,30 +38,45 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Cartographer creates {@link Map} from JSON encoded streams and encodes Maps to their JSON
- * representation.
+ * Cartographer creates {@link Map} objects from JSON encoded streams and decodes {@link Map}
+ * objects into JSON streams. Use {@link Builder} to construct instances.
  */
-class Cartographer {
+public class Cartographer {
+  static final Cartographer INSTANCE = new Builder().lenient(true).prettyPrint(false).build();
 
-  static final Cartographer INSTANCE = new Cartographer();
+  private final boolean isLenient;
+  private final boolean prettyPrint;
 
-  private Cartographer() {
+  Cartographer(boolean isLenient, boolean prettyPrint) {
+    this.isLenient = isLenient;
+    this.prettyPrint = prettyPrint;
   }
 
   /**
    * Deserializes the specified json into a {@link Map}. If you have the Json in a {@link Reader}
    * form instead of a {@link String}, use {@link #fromJson(Reader)} instead.
    */
-  Map<String, Object> fromJson(String json) throws IOException {
+  public Map<String, Object> fromJson(String json) throws IOException {
+    if (json == null) {
+      throw new IllegalArgumentException("json == null");
+    }
+    if (json.length() == 0) {
+      throw new IllegalArgumentException("json empty");
+    }
     return fromJson(new StringReader(json));
   }
 
   /**
-   * Deserializes the json read from the specified {@link Reader} into a {@link Map}. If you have
-   * the Json in a String form instead of a {@link Reader}, use {@link #fromJson(String)} instead.
+   * Deserializes the json read from the specified {@link Reader} into a {@link Map}.
+   * If you have the Json in a String form instead of a {@link Reader}, use {@link
+   * #fromJson(String)} instead.
    */
-  Map<String, Object> fromJson(Reader reader) throws IOException {
+  public Map<String, Object> fromJson(Reader reader) throws IOException {
+    if (reader == null) {
+      throw new IllegalArgumentException("reader == null");
+    }
     JsonReader jsonReader = new JsonReader(reader);
+    jsonReader.setLenient(isLenient);
     try {
       return readerToMap(jsonReader);
     } finally {
@@ -74,22 +89,33 @@ class Cartographer {
    * write the json to {@link Writer} instead of retrieving it as a String, use {@link #toJson(Map,
    * Writer)} instead.
    */
-  String toJson(Map<?, ?> map) throws IOException {
+  public String toJson(Map<?, ?> map) {
     StringWriter stringWriter = new StringWriter();
-    toJson(map, stringWriter);
+    try {
+      toJson(map, stringWriter);
+    } catch (IOException e) {
+      throw new AssertionError(e); // No I/O writing to a Buffer.
+    }
     return stringWriter.toString();
   }
 
   /**
    * Serializes the map into it's json representation into the provided {@link Writer}. If you want
-   * to retrieve the json as a string, use {@link #toJson(Map)} instead.
+   * to retrieve the json as a string,  use {@link #toJson(Map)} instead.
    */
-  void toJson(Map<?, ?> map, Writer writer) throws IOException {
+  public void toJson(Map<?, ?> map, Writer writer) throws IOException {
     if (map == null) {
       throw new IllegalArgumentException("map == null");
     }
+    if (writer == null) {
+      throw new IllegalArgumentException("writer == null");
+    }
 
     JsonWriter jsonWriter = new JsonWriter(writer);
+    jsonWriter.setLenient(isLenient);
+    if (prettyPrint) {
+      jsonWriter.setIndent("  ");
+    }
     try {
       mapToWriter(map, jsonWriter);
     } finally {
@@ -101,7 +127,7 @@ class Cartographer {
 
   /** Reads the {@link JsonReader} into a {@link Map}. */
   private static Map<String, Object> readerToMap(JsonReader reader) throws IOException {
-    Map<String, Object> map = new LinkedHashMap<>();
+    Map<String, Object> map = new LinkedHashMap<String, Object>();
     reader.beginObject();
     while (reader.hasNext()) {
       map.put(reader.nextName(), readValue(reader));
@@ -113,7 +139,7 @@ class Cartographer {
   /** Reads the {@link JsonReader} into a {@link List}. */
   private static List<Object> readerToList(JsonReader reader) throws IOException {
     // todo: try to infer the type of the List?
-    List<Object> list = new ArrayList<>();
+    List<Object> list = new ArrayList<Object>();
     reader.beginArray();
     while (reader.hasNext()) {
       list.add(readValue(reader));
@@ -176,6 +202,8 @@ class Cartographer {
 
   /**
    * Writes the given {@link Object} to the {@link JsonWriter}.
+   *
+   * @throws IOException
    */
   private static void writeValue(Object value, JsonWriter writer) throws IOException {
     if (value == null) {
@@ -186,12 +214,43 @@ class Cartographer {
       writer.value((Boolean) value);
     } else if (value instanceof List) {
       listToWriter((List) value, writer);
-    } else if (value.getClass().isArray()) {
-      arrayToWriter((Object[]) value, writer);
     } else if (value instanceof Map) {
       mapToWriter((Map) value, writer);
+    } else if (value.getClass().isArray()) {
+      arrayToWriter((Object[]) value, writer);
     } else {
       writer.value(String.valueOf(value));
+    }
+  }
+
+  /** Fluent API to construct instances of {@link Cartographer}. */
+  public static class Builder {
+    private boolean isLenient;
+    private boolean prettyPrint;
+
+    /**
+     * Configure this parser to be  be liberal in what it accepts. By default,
+     * this parser is strict and only accepts JSON as specified by <a
+     * href="http://www.ietf.org/rfc/rfc4627.txt">RFC 4627</a>. See {@link
+     * JsonReader#setLenient(boolean)} for more details.
+     * </ul>
+     */
+    public Builder lenient(boolean isLenient) {
+      this.isLenient = isLenient;
+      return this;
+    }
+
+    /**
+     * Configures Cartographer to output Json that fits in a page for pretty printing. This option
+     * only affects Json serialization.
+     */
+    public Builder prettyPrint(boolean prettyPrint) {
+      this.prettyPrint = prettyPrint;
+      return this;
+    }
+
+    public Cartographer build() {
+      return new Cartographer(isLenient, prettyPrint);
     }
   }
 }
--- analytics/src/test/java/com/segment/analytics/CartographerTest.java
+++ analytics/src/test/java/com/segment/analytics/CartographerTest.java
@@ -1,35 +1,343 @@
 package com.segment.analytics;
 
+import com.google.common.collect.ImmutableMap;
 import com.segment.analytics.core.BuildConfig;
-import com.segment.analytics.integrations.BasePayload;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.UUID;
+import org.assertj.core.data.MapEntry;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import static com.segment.analytics.TestUtils.TRACK_PAYLOAD;
-import static com.segment.analytics.TestUtils.TRACK_PAYLOAD_JSON;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 import static org.robolectric.annotation.Config.NONE;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 18, manifest = NONE)
+@Config(constants = BuildConfig.class, sdk = 18, manifest = NONE) //
 public class CartographerTest {
+  Cartographer cartographer;
 
-  @Test public void testSerialization() throws IOException {
-    assertThat(Cartographer.INSTANCE.toJson(TRACK_PAYLOAD)).isEqualTo(TRACK_PAYLOAD_JSON);
+  @Before public void setUp() {
+    cartographer = new Cartographer.Builder().lenient(false).prettyPrint(true).build();
   }
 
-  @Test public void testDeserialization() throws IOException {
-    Map<String, Object> map = Cartographer.INSTANCE.fromJson(TRACK_PAYLOAD_JSON);
+  @Test public void encodesPrimitives() throws IOException {
+    Map<String, Object> map = ImmutableMap.<String, Object>builder().put("byte", (byte) 32)
+        .put("boolean", true)
+        .put("short", (short) 100)
+        .put("int", 1)
+        .put("long", 43L)
+        .put("float", 23f)
+        .put("double", Math.PI)
+        .put("char", 'a')
+        .put("String", "string")
+        .build();
 
-    // special consideration for enums
-    assertThat(map).containsEntry("type", "track").containsEntry("channel", "mobile");
-    map.put("type", BasePayload.Type.track);
-    map.put("channel", BasePayload.Channel.mobile);
+    assertThat(cartographer.toJson(map)).isEqualTo("{\n"
+        + "  \"byte\": 32,\n"
+        + "  \"boolean\": true,\n"
+        + "  \"short\": 100,\n"
+        + "  \"int\": 1,\n"
+        + "  \"long\": 43,\n"
+        + "  \"float\": 23.0,\n"
+        + "  \"double\": 3.141592653589793,\n"
+        + "  \"char\": \"a\",\n"
+        + "  \"String\": \"string\"\n"
+        + "}");
+  }
 
-    assertThat(map).isEqualTo(TRACK_PAYLOAD);
+  @Test public void decodesPrimitives() throws IOException {
+    String json = "{\n"
+        + "  \"byte\": 32,\n"
+        + "  \"boolean\": true,\n"
+        + "  \"short\": 100,\n"
+        + "  \"int\": 1,\n"
+        + "  \"long\": 43,\n"
+        + "  \"float\": 23.0,\n"
+        + "  \"double\": 3.141592653589793,\n"
+        + "  \"char\": \"a\",\n"
+        + "  \"String\": \"string\"\n"
+        + "}";
+
+    Map<String, Object> map = cartographer.fromJson(json);
+
+    assertThat(map).hasSize(9)
+        .contains(MapEntry.entry("byte", 32.0))
+        .contains(MapEntry.entry("boolean", true))
+        .contains(MapEntry.entry("short", 100.0))
+        .contains(MapEntry.entry("int", 1.0))
+        .contains(MapEntry.entry("long", 43.0))
+        .contains(MapEntry.entry("float", 23.0))
+        .contains(MapEntry.entry("double", Math.PI))
+        .contains(MapEntry.entry("char", "a"))
+        .contains(MapEntry.entry("String", "string"));
+  }
+
+  @Test public void prettyPrintDisabled() throws IOException {
+    Cartographer cartographer = new Cartographer.Builder().prettyPrint(false).build();
+    Map<String, Object> map = ImmutableMap.<String, Object>builder().put("a",
+        ImmutableMap.<String, Object>builder().put("b",
+            ImmutableMap.<String, Object>builder().put("c",
+                ImmutableMap.<String, Object>builder().put("d",
+                    ImmutableMap.<String, Object>builder().put("e", "f").build()).build()).build())
+            .build()).build();
+
+    assertThat(cartographer.toJson(map)) //
+        .isEqualTo("{\"a\":{\"b\":{\"c\":{\"d\":{\"e\":\"f\"}}}}}");
+  }
+
+  @Test public void encodesNestedMaps() throws IOException {
+    Map<String, Object> map = ImmutableMap.<String, Object>builder().put("a",
+        ImmutableMap.<String, Object>builder().put("b",
+            ImmutableMap.<String, Object>builder().put("c",
+                ImmutableMap.<String, Object>builder().put("d",
+                    ImmutableMap.<String, Object>builder().put("e", "f").build()).build()).build())
+            .build()).build();
+
+    assertThat(cartographer.toJson(map)).isEqualTo("{\n"
+        + "  \"a\": {\n"
+        + "    \"b\": {\n"
+        + "      \"c\": {\n"
+        + "        \"d\": {\n"
+        + "          \"e\": \"f\"\n"
+        + "        }\n"
+        + "      }\n"
+        + "    }\n"
+        + "  }\n"
+        + "}");
+  }
+
+  @Test public void decodesNestedMaps() throws IOException {
+    String json = "{\n"
+        + "  \"a\": {\n"
+        + "    \"b\": {\n"
+        + "      \"c\": {\n"
+        + "        \"d\": {\n"
+        + "          \"e\": \"f\"\n"
+        + "        }\n"
+        + "      }\n"
+        + "    }\n"
+        + "  }\n"
+        + "}";
+
+    Map<String, Object> map = cartographer.fromJson(json);
+    Map<String, Object> expected = ImmutableMap.<String, Object>builder().put("a",
+        ImmutableMap.<String, Object>builder().put("b",
+            ImmutableMap.<String, Object>builder().put("c",
+                ImmutableMap.<String, Object>builder().put("d",
+                    ImmutableMap.<String, Object>builder().put("e", "f").build()).build()).build())
+            .build()).build();
+
+    assertThat(map).isEqualTo(expected);
+  }
+
+  @Test public void encodesArraysWithLists() throws IOException {
+    Map<String, Object> map =
+        ImmutableMap.<String, Object>builder().put("a", Arrays.asList("b", "c", "d")).build();
+
+    assertThat(cartographer.toJson(map)).isEqualTo("{\n" //
+        + "  \"a\": [\n" //
+        + "    \"b\",\n" //
+        + "    \"c\",\n" //
+        + "    \"d\"\n" //
+        + "  ]\n" //
+        + "}");
+  }
+
+  @Test public void decodesArraysWithLists() throws IOException {
+    String json = "{\n" //
+        + "  \"a\": [\n" //
+        + "    \"b\",\n" //
+        + "    \"c\",\n" //
+        + "    \"d\"\n" //
+        + "  ]\n" //
+        + "}";
+
+    Map<String, Object> expected =
+        ImmutableMap.<String, Object>builder().put("a", Arrays.asList("b", "c", "d")).build();
+
+    assertThat(cartographer.fromJson(json)).isEqualTo(expected);
+  }
+
+  @Test public void encodesArraysWithArrays() throws IOException {
+    Map<String, Object> map =
+        ImmutableMap.<String, Object>builder().put("a", new String[] { "b", "c", "d" }).build();
+
+    assertThat(cartographer.toJson(map)).isEqualTo("{\n" //
+        + "  \"a\": [\n" //
+        + "    \"b\",\n" //
+        + "    \"c\",\n" //
+        + "    \"d\"\n" //
+        + "  ]\n" //
+        + "}");
+  }
+
+  @Test public void decodesArraysWithArraysAsLists() throws IOException {
+    String json = "{\n" //
+        + "  \"a\": [\n" //
+        + "    \"b\",\n" //
+        + "    \"c\",\n" //
+        + "    \"d\"\n" //
+        + "  ]\n" //
+        + "}";
+
+    Map<String, Object> expected =
+        ImmutableMap.<String, Object>builder().put("a", Arrays.asList("b", "c", "d")).build();
+
+    assertThat(cartographer.fromJson(json)).isEqualTo(expected);
+  }
+
+  @Test public void encodesArrayOfMap() throws IOException {
+    Map<String, Object> map = ImmutableMap.<String, Object>builder().put("a",
+        Arrays.<ImmutableMap>asList(ImmutableMap.<String, Object>builder().put("b", "c").build(),
+            ImmutableMap.<String, Object>builder().put("b", "d").build(),
+            ImmutableMap.<String, Object>builder().put("b", "e").build())).build();
+
+    assertThat(cartographer.toJson(map)).isEqualTo("{\n"
+        + "  \"a\": [\n"
+        + "    {\n"
+        + "      \"b\": \"c\"\n"
+        + "    },\n"
+        + "    {\n"
+        + "      \"b\": \"d\"\n"
+        + "    },\n"
+        + "    {\n"
+        + "      \"b\": \"e\"\n"
+        + "    }\n"
+        + "  ]\n"
+        + "}");
+  }
+
+  @Test public void decodesArrayOfMap() throws IOException {
+    String json = "{\n"
+        + "  \"a\": [\n"
+        + "    {\n"
+        + "      \"b\": \"c\"\n"
+        + "    },\n"
+        + "    {\n"
+        + "      \"b\": \"d\"\n"
+        + "    },\n"
+        + "    {\n"
+        + "      \"b\": \"e\"\n"
+        + "    }\n"
+        + "  ]\n"
+        + "}";
+
+    Map<String, Object> expected = ImmutableMap.<String, Object>builder().put("a",
+        Arrays.<ImmutableMap>asList(ImmutableMap.<String, Object>builder().put("b", "c").build(),
+            ImmutableMap.<String, Object>builder().put("b", "d").build(),
+            ImmutableMap.<String, Object>builder().put("b", "e").build())).build();
+
+    assertThat(cartographer.fromJson(json)).isEqualTo(expected);
+  }
+
+  @Test public void disallowsEncodingNullMap() throws IOException {
+    try {
+      cartographer.toJson(null, new StringWriter());
+      fail("null map should throw Exception");
+    } catch (IllegalArgumentException e) {
+      assertThat(e).hasMessage("map == null");
+    }
+  }
+
+  @Test public void disallowsEncodingToNullWriter() throws IOException {
+    try {
+      cartographer.toJson(new LinkedHashMap<Object, Object>(), null);
+      fail("null writer should throw Exception");
+    } catch (IllegalArgumentException e) {
+      assertThat(e).hasMessage("writer == null");
+    }
+  }
+
+  @Test public void disallowsDecodingNullReader() throws IOException {
+    try {
+      cartographer.fromJson((Reader) null);
+      fail("null map should throw Exception");
+    } catch (IllegalArgumentException e) {
+      assertThat(e).hasMessage("reader == null");
+    }
+  }
+
+  @Test public void disallowsDecodingNullString() throws IOException {
+    try {
+      cartographer.fromJson((String) null);
+      fail("null map should throw Exception");
+    } catch (IllegalArgumentException e) {
+      assertThat(e).hasMessage("json == null");
+    }
+  }
+
+  @Test public void disallowsDecodingEmptyString() throws IOException {
+    try {
+      cartographer.fromJson("");
+      fail("null map should throw Exception");
+    } catch (IllegalArgumentException e) {
+      assertThat(e).hasMessage("json empty");
+    }
+  }
+
+  @Test public void encodesNumberMax() throws IOException {
+    StringWriter writer = new StringWriter();
+    Map<String, Object> map = new LinkedHashMap<String, Object>();
+    map.put("byte", Byte.MAX_VALUE);
+    map.put("short", Short.MAX_VALUE);
+    map.put("int", Integer.MAX_VALUE);
+    map.put("long", Long.MAX_VALUE);
+    map.put("float", Float.MAX_VALUE);
+    map.put("double", Double.MAX_VALUE);
+    map.put("char", Character.MAX_VALUE);
+
+    cartographer.toJson(map, writer);
+
+    assertThat(writer.toString()).isEqualTo("{\n"
+        + "  \"byte\": 127,\n"
+        + "  \"short\": 32767,\n"
+        + "  \"int\": 2147483647,\n"
+        + "  \"long\": 9223372036854775807,\n"
+        + "  \"float\": 3.4028235E38,\n"
+        + "  \"double\": 1.7976931348623157E308,\n"
+        + "  \"char\": \"\uFFFF\"\n"
+        + "}");
+  }
+
+  @Test public void encodesNumberMin() throws IOException {
+    StringWriter writer = new StringWriter();
+    Map<String, Object> map = new LinkedHashMap<String, Object>();
+    map.put("byte", Byte.MIN_VALUE);
+    map.put("short", Short.MIN_VALUE);
+    map.put("int", Integer.MIN_VALUE);
+    map.put("long", Long.MIN_VALUE);
+    map.put("float", Float.MIN_VALUE);
+    map.put("double", Double.MIN_VALUE);
+    map.put("char", Character.MIN_VALUE);
+
+    cartographer.toJson(map, writer);
+
+    assertThat(writer.toString()).isEqualTo("{\n"
+        + "  \"byte\": -128,\n"
+        + "  \"short\": -32768,\n"
+        + "  \"int\": -2147483648,\n"
+        + "  \"long\": -9223372036854775808,\n"
+        + "  \"float\": 1.4E-45,\n"
+        + "  \"double\": 4.9E-324,\n"
+        + "  \"char\": \"\\u0000\"\n"
+        + "}");
+  }
+
+  @Test public void encodesLargeDocuments() throws IOException {
+    Map<String, Object> map = new LinkedHashMap<String, Object>();
+    for (int i = 0; i < 100; i++) {
+      map.put(UUID.randomUUID().toString(), UUID.randomUUID().toString());
+    }
+    StringWriter writer = new StringWriter();
+    cartographer.toJson(map, writer);
   }
 }
