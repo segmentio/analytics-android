cdec2cf25af221a616c722bf95c62d20b1417fa2
Prateek Srivastava <f2prateek@gmail.com>
2018-07-16 17:04:54 -0700

    Generate license headers with spotless

--- analytics/src/main/java/com/segment/analytics/Options.java
+++ analytics/src/main/java/com/segment/analytics/Options.java
@@ -1,7 +1,7 @@
-/*
+/**
  * The MIT License (MIT)
  *
- * Copyright (c) 2014 Segment, Inc.
+ * Copyright (c) 2014 Segment.io, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
 package com.segment.analytics;
 
 import java.util.LinkedHashMap;
