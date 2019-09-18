af022014f834a609d43cb9eb951dd6afa1d59dcf
Jason Gavris <jgavris@butterflynetinc.com>
2019-09-18 11:01:13 -0400

    Make Crypto abstract methods public

--- analytics/src/main/java/com/segment/analytics/Crypto.java
+++ analytics/src/main/java/com/segment/analytics/Crypto.java
@@ -27,19 +27,19 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public abstract class Crypto {
-  abstract InputStream decrypt(InputStream is);
+  public abstract InputStream decrypt(InputStream is);
 
-  abstract OutputStream encrypt(OutputStream os);
+  public abstract OutputStream encrypt(OutputStream os);
 
   public static Crypto none() {
     return new Crypto() {
       @Override
-      InputStream decrypt(InputStream is) {
+      public InputStream decrypt(InputStream is) {
         return is;
       }
 
       @Override
-      OutputStream encrypt(OutputStream os) {
+      public OutputStream encrypt(OutputStream os) {
         return os;
       }
     };
