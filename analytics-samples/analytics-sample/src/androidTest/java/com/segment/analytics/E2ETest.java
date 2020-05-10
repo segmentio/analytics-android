94f8fa74b254f5603f80e7cb577748ae7774e125
Andrew Lord <andrew@lordcodes.com>
2020-05-10 11:58:17 +0100

    Update the sample end to end test

--- analytics-samples/analytics-sample/src/androidTest/java/com/segment/analytics/E2ETest.java
+++ analytics-samples/analytics-sample/src/androidTest/java/com/segment/analytics/E2ETest.java
@@ -26,10 +26,10 @@ package com.segment.analytics;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.fail;
 
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+import com.segment.analytics.sample.BuildConfig;
 import com.segment.analytics.sample.MainActivity;
-import com.segment.analytics.sample.test.BuildConfig;
 import com.segment.analytics.webhook.WebhookService;
 import com.segment.backo.Backo;
 import java.io.IOException;
