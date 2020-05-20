4e9ed06245aa5862737892c7cb3e806c7b1d6e57
Prayansh Srivastava <prayansh@gmail.com>
2020-05-20 16:26:24 -0700

    run formatter

--- analytics/src/main/java/com/segment/analytics/Analytics.java
+++ analytics/src/main/java/com/segment/analytics/Analytics.java
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * <p>
+ *
  * Copyright (c) 2014 Segment.io, Inc.
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -1258,11 +1258,10 @@ public class Analytics {
     }
 
     /**
-     * Set the default project settings to use, if Segment.com cannot be reached.
-     * An example configuration can be found here, using your write key:
-     * <a href="https://cdn-settings.segment.com/v1/projects/YOUR_WRITE_KEY/settings">
-     *   https://cdn-settings.segment.com/v1/projects/YOUR_WRITE_KEY/settings
-     * </a>
+     * Set the default project settings to use, if Segment.com cannot be reached. An example
+     * configuration can be found here, using your write key: <a
+     * href="https://cdn-settings.segment.com/v1/projects/YOUR_WRITE_KEY/settings">
+     * https://cdn-settings.segment.com/v1/projects/YOUR_WRITE_KEY/settings </a>
      */
     public Builder defaultProjectSettings(ValueMap defaultIntegrationSettings) {
       assertNotNull(defaultIntegrationSettings, "defaultIntegrationSettings");
