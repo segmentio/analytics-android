40e36dfeaaac6a1474a4992f6deadd41d3bd2347
Jessica-Kuo <jkuo3@binghamton.edu>
2020-07-27 18:43:35 -0400

    converted CryptoTest to Kotlin

---
+++ analytics/src/test/java/com/segment/analytics/CryptoTest.kt
@@ -0,0 +1,40 @@
+package com.segment.analytics
+
+import okio.Buffer
+import okio.ByteString
+import okio.Okio
+import org.assertj.core.api.Assertions
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import java.io.IOException
+
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = Config.NONE)
+class CryptoTest{
+
+    @Throws(IOException::class)
+    @Test
+    fun noneCryptoWrite() {
+        val crypto: Crypto = Crypto.none()
+        val foo: ByteString = ByteString.encodeUtf8("foo")
+        val buffer = Buffer()
+
+        foo.write(crypto.encrypt(buffer.outputStream()))
+
+        Assertions.assertThat(buffer.readByteString()).isEqualTo(foo)
+    }
+
+    @Test
+    fun cryptoRead() {
+        val crypto: Crypto = Crypto.none()
+        val foo: ByteString = ByteString.encodeUtf8("foo")
+        val buffer = Buffer()
+
+        buffer.write(foo)
+
+        Assertions.assertThat(Okio.buffer(Okio.source(crypto.decrypt(buffer.inputStream()))).readByteString())
+                .isEqualTo(foo)
+    }
+}
