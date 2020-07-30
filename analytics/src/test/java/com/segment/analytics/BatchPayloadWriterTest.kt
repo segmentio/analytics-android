f129d4d72fb30ed3ce6772737b75992b240c73c1
Jessica Kuo <jkuo3@binghamton.edu>
2020-07-30 12:26:53 -0400

    converted BatchPayloadWriterTest to Kotlin (#682)

---
+++ analytics/src/test/java/com/segment/analytics/BatchPayloadWriterTest.kt
@@ -0,0 +1,67 @@
+package com.segment.analytics
+
+import com.segment.analytics.SegmentIntegration.BatchPayloadWriter
+import org.assertj.core.api.Assertions
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+
+
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = Config.NONE)
+class BatchPayloadWriterTest {
+  @Test
+  @Throws(IOException::class)
+  fun batchPayloadWriter() {
+    val byteArrayOutputStream = ByteArrayOutputStream()
+    val batchPayloadWriter = BatchPayloadWriter(byteArrayOutputStream)
+    batchPayloadWriter
+        .beginObject()
+        .beginBatchArray()
+        .emitPayloadObject("foobarbazqux")
+        .emitPayloadObject("{}")
+        .emitPayloadObject("2")
+        .endBatchArray()
+        .endObject()
+        .close()
+
+    // todo: inject a fake clock. for now we'll compare a lower precision.
+    Assertions.assertThat(byteArrayOutputStream.toString())
+        .contains("{\"batch\":[foobarbazqux,{},2],\"sentAt\":\"")
+  }
+
+  @Throws(IOException::class)
+  @Test
+  fun batchPayloadWriterSingleItem() {
+    val byteArrayOutputStream = ByteArrayOutputStream()
+    val batchPayloadWriter = BatchPayloadWriter(byteArrayOutputStream)
+    batchPayloadWriter
+        .beginObject()
+        .beginBatchArray()
+        .emitPayloadObject("qaz")
+        .endBatchArray()
+        .endObject()
+        .close()
+
+    // todo: inject a fake clock. for now we'll compare a lower precision.
+    Assertions.assertThat(byteArrayOutputStream.toString())
+        .contains("{\"batch\":[qaz],\"sentAt\":\"")
+  }
+
+  @Throws(IOException::class)
+  @Test
+  fun batchPayloadWriterFailsForNoItem() {
+    val byteArrayOutputStream = ByteArrayOutputStream()
+    val batchPayloadWriter = BatchPayloadWriter(byteArrayOutputStream)
+
+    try {
+      batchPayloadWriter.beginObject().beginBatchArray().endBatchArray().endObject().close()
+    } catch (exception: IOException) {
+      Assertions.assertThat(exception).hasMessage("At least one payload must be provided.")
+    }
+  }
+
+}
