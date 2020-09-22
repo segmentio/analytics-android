b05fdc13500302391d8da6caa9595dc684c93f3e
Jessica-Kuo <jessica.kuo@segment.com>
2020-09-22 19:23:52 -0400

    converted SourceMiddlewareTest to Kotlin

---
+++ analytics/src/test/java/com/segment/analytics/SourceMiddlewareTest.kt
@@ -0,0 +1,111 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Segment.io, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.segment.analytics
+
+import android.Manifest
+import com.google.common.util.concurrent.MoreExecutors
+import com.segment.analytics.integrations.BasePayload
+import com.segment.analytics.integrations.ScreenPayload
+import com.segment.analytics.integrations.TrackPayload
+import java.util.concurrent.atomic.AtomicReference
+import kotlin.jvm.Throws
+import org.assertj.core.api.Assertions
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.MockitoAnnotations
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = Config.NONE)
+class SourceMiddlewareTest {
+
+    lateinit var builder: Analytics.Builder
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        Analytics.INSTANCES.clear()
+        TestUtils.grantPermission(RuntimeEnvironment.application, Manifest.permission.INTERNET)
+        builder =
+            Analytics.Builder(RuntimeEnvironment.application, "write_key")
+                .executor(MoreExecutors.newDirectExecutorService())
+    }
+
+    @Throws(Exception::class)
+    @Test
+    fun middlewareCanShortCircuit() {
+        val payloadRef = AtomicReference<TrackPayload>()
+        val analytics: Analytics =
+            builder
+                .useSourceMiddleware(
+                    fun(chain: Middleware.Chain) {
+                        payloadRef.set(chain.payload() as TrackPayload)
+                    }
+                )
+                .useSourceMiddleware {
+                    fun intercept(chain: Middleware.Chain) {
+                        throw AssertionError("should not be invoked")
+                    }
+                }
+                .build()
+
+        analytics.track("foo")
+        Assertions.assertThat(payloadRef.get().event()).isEqualTo("foo")
+    }
+
+    @Throws(Exception::class)
+    @Test
+    fun middlewareCanProceed() {
+        val payloadRef = AtomicReference<ScreenPayload>()
+        val analytics = builder
+            .useSourceMiddleware { chain -> chain.proceed(chain.payload()) }
+            .useSourceMiddleware { chain ->
+                val payload = chain.payload()
+                payloadRef.set(payload as ScreenPayload)
+                chain.proceed(payload)
+            }
+            .build()
+        analytics.screen("foo")
+        Assertions.assertThat(payloadRef.get().name()).isEqualTo("foo")
+    }
+
+    @Test
+    @Throws(java.lang.Exception::class)
+    fun middlewareCanTransform() {
+        val payloadRef = AtomicReference<BasePayload>()
+        val analytics = builder
+            .useSourceMiddleware { chain -> chain.proceed(chain.payload().toBuilder().messageId("override").build()) }
+            .useSourceMiddleware { chain ->
+                val payload = chain.payload()
+                payloadRef.set(payload)
+                chain.proceed(payload)
+            }
+            .build()
+        analytics.identify("prateek")
+        Assertions.assertThat(payloadRef.get().messageId()).isEqualTo("override")
+    }
+}
