54dc9f17e634483e9d7224734950ea04f5e35531
Prayansh Srivastava <prayansh@gmail.com>
2020-12-18 15:26:22 -0800

    comment out new event types due to name collision

--- analytics/src/main/java/com/segment/analytics/BaseEvent.kt
+++ analytics/src/main/java/com/segment/analytics/BaseEvent.kt
@@ -25,71 +25,71 @@ class DateSerializer : KSerializer<NanoDate> {
 
 }
 
-typealias Context = JsonObject
-typealias Integrations = JsonObject
-typealias Properties = JsonObject
-typealias Traits = JsonObject
-
-abstract class BaseEvent {
-    abstract var anonymousId: String
-    abstract var messageId: String
-    abstract var timestamp: NanoDate
-    abstract var context: Context
-    abstract var integrations: Integrations
-}
-
-@Serializable
-data class TrackEvent(
-        override var anonymousId: String,
-        override var messageId: String,
-        @Serializable(with = DateSerializer::class) override var timestamp: NanoDate,
-        override var context: Context,
-        override var integrations: Integrations,
-        var properties: Properties
-) : BaseEvent()
-
-@Serializable
-data class IdentifyEvent(
-        override var anonymousId: String,
-        override var messageId: String,
-        @Serializable(with = DateSerializer::class) override var timestamp: NanoDate,
-        override var context: Context,
-        override var integrations: Integrations,
-        var traits: Traits,
-        var userId: String
-) : BaseEvent()
-
-
-@Serializable
-data class GroupEvent(
-        override var anonymousId: String,
-        override var messageId: String,
-        @Serializable(with = DateSerializer::class) override var timestamp: NanoDate,
-        override var context: Context,
-        override var integrations: Integrations,
-        var traits: Traits,
-        var groupId: String
-) : BaseEvent()
-
-@Serializable
-data class Alias(
-        override var anonymousId: String,
-        override var messageId: String,
-        @Serializable(with = DateSerializer::class) override var timestamp: NanoDate,
-        override var context: Context,
-        override var integrations: Integrations,
-        var previousId: String,
-        var userId: String
-) : BaseEvent()
-
-@Serializable
-data class ScreenEvent(
-        override var anonymousId: String,
-        override var messageId: String,
-        @Serializable(with = DateSerializer::class) override var timestamp: NanoDate,
-        override var context: Context,
-        override var integrations: Integrations,
-        var category: String,
-        var name: String,
-        var properties: Properties
-) : BaseEvent()
+//typealias Context = JsonObject
+//typealias Integrations = JsonObject
+//typealias Properties = JsonObject
+//typealias Traits = JsonObject
+//
+//abstract class BaseEvent {
+//    abstract var anonymousId: String
+//    abstract var messageId: String
+//    abstract var timestamp: NanoDate
+//    abstract var context: Context
+//    abstract var integrations: Integrations
+//}
+//
+//@Serializable
+//data class TrackEvent(
+//        override var anonymousId: String,
+//        override var messageId: String,
+//        @Serializable(with = DateSerializer::class) override var timestamp: NanoDate,
+//        override var context: Context,
+//        override var integrations: Integrations,
+//        var properties: Properties
+//) : BaseEvent()
+//
+//@Serializable
+//data class IdentifyEvent(
+//        override var anonymousId: String,
+//        override var messageId: String,
+//        @Serializable(with = DateSerializer::class) override var timestamp: NanoDate,
+//        override var context: Context,
+//        override var integrations: Integrations,
+//        var traits: Traits,
+//        var userId: String
+//) : BaseEvent()
+//
+//
+//@Serializable
+//data class GroupEvent(
+//        override var anonymousId: String,
+//        override var messageId: String,
+//        @Serializable(with = DateSerializer::class) override var timestamp: NanoDate,
+//        override var context: Context,
+//        override var integrations: Integrations,
+//        var traits: Traits,
+//        var groupId: String
+//) : BaseEvent()
+//
+//@Serializable
+//data class Alias(
+//        override var anonymousId: String,
+//        override var messageId: String,
+//        @Serializable(with = DateSerializer::class) override var timestamp: NanoDate,
+//        override var context: Context,
+//        override var integrations: Integrations,
+//        var previousId: String,
+//        var userId: String
+//) : BaseEvent()
+//
+//@Serializable
+//data class ScreenEvent(
+//        override var anonymousId: String,
+//        override var messageId: String,
+//        @Serializable(with = DateSerializer::class) override var timestamp: NanoDate,
+//        override var context: Context,
+//        override var integrations: Integrations,
+//        var category: String,
+//        var name: String,
+//        var properties: Properties
+//) : BaseEvent()
